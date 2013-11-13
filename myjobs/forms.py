6729e58e746a61c9b1dd4d869995d539c9e8503e
tdphillips <troy@directemployersfoundation.org>
2013-11-13 09:23:30 -0500

    PEP 8 formatting [MJ-649]

--- myjobs/forms.py
+++ myjobs/forms.py
@@ -5,6 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 from myjobs.models import User
 from myprofile.models import Name, SecondaryEmail
 
+
 def make_choices(user, default_value='', default_text=''):
     """
     Turns the set of email addresses associated with a user into a list
@@ -25,56 +26,59 @@ def make_choices(user, default_value='', default_text=''):
             choices.append((email.email, email.email))
     return choices
 
+
 class BaseUserForm(ModelForm):
     """
     Most models in the other apps are associated with a user. This base form
     will take a user object as a key word argument and saves the form instance
-    to a that specified user. It also takes a few common inputs that can be used
-    to customize form rendering.
+    to a that specified user. It also takes a few common inputs that can be
+    used to customize form rendering.
 
     Inputs (these are the common inputs we will use for rendering forms):
-    :user:       a user object. We will always pass a user object in  because all
-                 ProfileUnits are linked to a user.
-    :auto_id:    this is a boolean that determines whether a label is displayed or
-                 not and is by default set to True. Setting this to false uses the
-                 placeholder text instead, except for boolean and select fields.
-    :empty_permitted: allow form to be submitted as empty even if the fields are
-                 required. This is particularly useful when we combine multiple
-                 Django forms on the front end and submit it as one request instead
-                 of several separate requests.
-    :only_show_required: Template uses this flag to determine if it should only render
-                 required forms. Default is False.
+    :user: a user object. We will always pass a user object in  because all
+        ProfileUnits are linked to a user.
+    :auto_id: this is a boolean that determines whether a label is displayed or
+        not and is by default set to True. Setting this to false uses the
+        placeholder text instead, except for boolean and select fields.
+    :empty_permitted: allow form to be submitted as empty even if the fields
+        are required. This is particularly useful when we combine multiple
+        Django forms on the front end and submit it as one request instead of
+        several separate requests.
+    :only_show_required: Template uses this flag to determine if it should only
+        render required forms. Default is False.
     """
 
     def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('user',None)
-        self.only_show_required = kwargs.pop('only_show_required',False)
-        super (BaseUserForm, self).__init__(*args, **kwargs)
+        self.user = kwargs.pop('user', None)
+        self.only_show_required = kwargs.pop('only_show_required', False)
+        super(BaseUserForm, self).__init__(*args, **kwargs)
 
     def save(self, commit=True):
         instance = super(BaseUserForm, self).save(commit=False)
         if self.user and not self.user.is_anonymous():
             instance.user = self.user
             return instance.save()
-    
+
 
 class EditAccountForm(Form):
     given_name = CharField(label=_("First Name"),
-                            widget=TextInput(attrs={'placeholder': 'First Name'}),
-                                 max_length=40, required=False)
+                           widget=TextInput(
+                               attrs={'placeholder': 'First Name'}),
+                           max_length=40, required=False)
     family_name = CharField(label=_("Last Name"),
-                            widget=TextInput(attrs={'placeholder': 'Last Name'}),
-                                max_length=40, required=False)
+                            widget=TextInput(
+                                attrs={'placeholder': 'Last Name'}),
+                            max_length=40, required=False)
 
     def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('user',None)
+        self.user = kwargs.pop('user', None)
         self.choices = make_choices(self.user, 'none', 'Do not use Gravatar')
         super(EditAccountForm, self).__init__(*args, **kwargs)
-        self.fields["gravatar"] = ChoiceField(label=_("Gravatar Email"),
-                                              widget=Select(attrs=
-                                              {'id':'id_gravatar'}),
-                                              choices=self.choices,
-                                              initial=self.choices[0][0])
+        self.fields["gravatar"] = ChoiceField(
+            label=_("Gravatar Email"),
+            widget=Select(attrs={'id': 'id_gravatar'}),
+            choices=self.choices,
+            initial=self.choices[0][0])
 
     def clean(self):
         cleaned_data = super(EditAccountForm, self).clean()
@@ -91,9 +95,8 @@ class EditAccountForm(Form):
             # cleaned data.
             del cleaned_data["given_name"]
             del cleaned_data["family_name"]
-        
+
         return cleaned_data
-    
 
     def save(self, u):
         first = self.cleaned_data.get("given_name", None)
@@ -118,10 +121,10 @@ class EditAccountForm(Form):
 
 class EditCommunicationForm(BaseUserForm):
     def __init__(self, *args, **kwargs):
-        super(EditCommunicationForm,self).__init__(*args, **kwargs)
+        super(EditCommunicationForm, self).__init__(*args, **kwargs)
         choices = make_choices(self.user)
         self.fields["email"] = ChoiceField(widget=Select(attrs={
-                                           'id':'id_digest_email'}),
+                                           'id': 'id_digest_email'}),
                                            choices=choices,
                                            initial=choices[0][0])
         self.fields["email"].label = "Primary Email:"
@@ -143,14 +146,18 @@ class EditCommunicationForm(BaseUserForm):
 
 class ChangePasswordForm(Form):
     password = CharField(label="Password",
-                                widget=PasswordInput(attrs={'placeholder':'Password'}))
+                         widget=PasswordInput(
+                             attrs={'placeholder': 'Password'}))
     new_password1 = CharField(label="New Password",
-                                widget=PasswordInput(attrs={'placeholder':'New Password'}))
+                              widget=PasswordInput(
+                                  attrs={'placeholder': 'New Password'}))
     new_password2 = CharField(label="New Password (again)",
-                                widget=PasswordInput(attrs={'placeholder':'New Password (again)'}))
-    
-    def __init__(self,*args, **kwargs):
-        self.user = kwargs.pop('user',None)
+                              widget=PasswordInput(
+                                  attrs={'placeholder':
+                                         'New Password (again)'}))
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
         super(ChangePasswordForm, self).__init__(*args, **kwargs)
 
     def clean_password(self):
@@ -159,7 +166,7 @@ class ChangePasswordForm(Form):
             raise ValidationError(("Wrong password."))
         else:
             return self.cleaned_data['password']
-        
+
     def clean(self):
         cleaned_data = super(ChangePasswordForm, self).clean()
         if 'new_password1' in self.cleaned_data and 'new_password2' in self.cleaned_data:
@@ -172,10 +179,9 @@ class ChangePasswordForm(Form):
                 # cleaned data.
                 del cleaned_data["new_password1"]
                 del cleaned_data["new_password2"]
-                #raise forms.ValidationError(_("The two new password fields did not match."))
             else:
                 return self.cleaned_data
 
     def save(self):
         self.user.set_password(self.cleaned_data["new_password1"])
-        self.user.save()    
+        self.user.save()
