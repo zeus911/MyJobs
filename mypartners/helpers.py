b007fecabd36dc7bf151d14b9d80e41952b86e1c
almathew <ashley@directemployersfoundation.org>
2014-03-13 09:53:11 -0400

    missing import

--- mypartners/helpers.py
+++ mypartners/helpers.py
@@ -2,14 +2,14 @@ from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
 from django.core.mail import EmailMessage
-from django.shortcuts import get_object_or_404
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 from django.utils.text import get_text_list, force_unicode, force_text
 from django.utils.translation import ugettext
 
-from datetime import datetime, timedelta
+from datetime import datetime, time, timedelta
 import re
 from urlparse import urlparse, parse_qsl, urlunparse
 from urllib import urlencode
