bab756fda914e26e6a7f98172278457a53ef4b5a
mklauber <matt@directemployersfoundation.org>
2014-03-14 09:14:36 -0400

    Updated the search results template to display a number of results and the date range

--- mypartners/views.py
+++ mypartners/views.py
@@ -1,6 +1,6 @@
 from collections import OrderedDict
 import csv
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from email.parser import HeaderParser
 from email.utils import getaddresses, parsedate
 from itertools import chain
@@ -25,7 +25,8 @@ from django.views.decorators.csrf import csrf_exempt
 from myjobs.models import User
 from mydashboard.models import Company
 from mysearches.models import SavedSearch, PartnerSavedSearch
-from mysearches.helpers import url_sort_options, parse_feed
+from mysearches.helpers import (url_sort_options, parse_feed,
+                                get_interval_from_frequency)
 from mysearches.forms import PartnerSavedSearchForm
 from mypartners.forms import (PartnerForm, ContactForm, PartnerInitialForm,
                               NewPartnerForm, ContactRecordForm)
@@ -471,6 +472,8 @@ def partner_view_full_feed(request):
                                        saved_search.sort_by,
                                        saved_search.frequency)
         items, count = parse_feed(url_of_feed, saved_search.frequency)
+        start_date = date.today() + timedelta(get_interval_from_frequency(
+                                                    saved_search.frequency))
         extras = saved_search.partnersavedsearch.url_extras
         if extras:
             add_extra_params_to_jobs(items, extras)
@@ -485,6 +488,8 @@ def partner_view_full_feed(request):
         'is_pss': True,
         'partner': partner.id,
         'company': company.id,
+        'start_date': start_date,
+        'count': count
     }
 
     return render_to_response('mysearches/view_full_feed.html', ctx,
