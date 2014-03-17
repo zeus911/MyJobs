ea815df82984ea277156cb6af567b5af38e61471
almathew <ashley@directemployersfoundation.org>
2014-03-17 11:25:07 -0400

    Add pss email and subject to contact record and basic cleaning of email content for record viewing [MJ-783]

--- mypartners/templatetags/activity_tags.py
+++ mypartners/templatetags/activity_tags.py
@@ -1,5 +1,8 @@
+import bleach
+
 from django.template import Library
 from django.utils.encoding import force_text
+from django.utils.safestring import mark_safe
 
 from mypartners.helpers import get_attachment_link
 from mypartners.models import CONTACT_TYPE_CHOICES, ACTIVITY_TYPES
@@ -50,7 +53,19 @@ def get_action_type(activity):
     action_type = ACTIVITY_TYPES[activity.action_flag]
     return action_type.title()
 
+
 @register.simple_tag
 def get_contact_type(record):
     contact_type_choices = dict(CONTACT_TYPE_CHOICES)
     return contact_type_choices[record.contact_type]
+
+
+@register.filter
+def bleach_clean(string):
+    tags = ['table', 'a', 'p', 'th', 'tr', 'td', 'b', 'i', 'div', 'br', 'span']
+    attrs = {
+        '*': ['style', 'class', 'id'],
+        'a': ['href'],
+    }
+
+    return mark_safe(bleach.clean(string, tags, attrs, [], strip=True))
