0955a863adda526870fc9950ab3fd854e98aabe5
galitskyd <david@directemployersfoundation.org>
2014-04-11 10:16:32 -0400

    add check to make sure main column is larger than side column [MJ-735]

--- static/profile.153-15.js
+++ static/profile.153-15.js
@@ -285,38 +285,39 @@ $(document).ready(function() {
     if($(window).width() >= 501) {
         // This function will be executed when the user scrolls the page.
         $(window).scroll(function(e) {
-                // Get the position of the location where the scroller starts.
-                var scroller_anchor;
-                try {
-                    scroller_anchor = $(".scroller_anchor").offset().top;
-                } catch(e) {
-                    scroller_anchor = 0;
-                }
-     
-                // Check if the user has scrolled and the current position is after the scroller start location and if its not already fixed at the top 
-                if ($(this).scrollTop() >= scroller_anchor && $('.right-side-fixed').css('position') != 'fixed') 
-                {    // Change the CSS of the scroller to hilight it and fix it at the top of the screen.
+            // Get the position of the location where the scroller starts.
+            var scroller_anchor;
+            try {
+                scroller_anchor = $(".scroller_anchor").offset().top;
+            } catch(e) {
+                scroller_anchor = 0;
+            }
+
+            // only switch relative/fixed if the main column is larger than the right column
+            if($('#moduleColumn').height() > $('#rightColumn').height()) {
+                // Check if the user has scrolled and the current position is after the scroller start location and if its not already fixed at the top
+                if ($(this).scrollTop() >= scroller_anchor && $('.right-side-fixed').css('position') != 'fixed') {
+                    // Change the CSS of the scroller to hilight it and fix it at the top of the screen.
                     $('.right-side-fixed').css({
                             'width': '288px',
                             'position': 'fixed',
                             'top': '10px'
-                });
-                // Changing the height of the scroller anchor to that of scroller so that there is no change in the overall height of the page.
-                $('.scroller_anchor').css('height', '50px');
-                } 
-                else if ($(this).scrollTop() < scroller_anchor && $('.right-side-fixed').css('position') != 'relative') 
-                {    // If the user has scrolled back to the location above the scroller anchor place it back into the content.
-         
+                    });
+                    // Changing the height of the scroller anchor to that of scroller so that there is no change in the overall height of the page.
+                    $('.scroller_anchor').css('height', '50px');
+                } else if ($(this).scrollTop() < scroller_anchor && $('.right-side-fixed').css('position') != 'relative') {
+                    // If the user has scrolled back to the location above the scroller anchor place it back into the content
                     // Change the height of the scroller anchor to 0 and now we will be adding the scroller back to the content.
                     $('.scroller_anchor').css('height', '0px');
-         
+
                     // Change the CSS and put it back to its original position.
                     $('.right-side-fixed').css({
                             'width': 'auto',
                             'top': '0px',
                             'position': 'relative'
-                });
+                    });
                 }
+            }
         });
     }
 });
