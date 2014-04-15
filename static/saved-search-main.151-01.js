e7206214f767f90901aefe7c94cef1ddf859338a
galitskyd <david@directemployersfoundation.org>
2014-04-15 09:42:09 -0400

    backbone to strictly jQuery savedsearchmain [MJ-842]

--- static/saved-search-main.151-01.js
+++ static/saved-search-main.151-01.js
@@ -1,60 +1,10 @@
-$(function() {
-    var SearchView = Backbone.View.extend({
-        el: 'body',
+$(document).ready(function() {
+    $('#digest_submit').click(function(e){
+        e.preventDefault();
+        var form = $(e.target).parents('#digest-option');
 
-        events: {
-            'click [id="digest_submit"]': 'check_digest_options',
-        },
-
-
-        check_digest_options: function(e) {
-            // When the user interacts with any fields in the Digest Options form,
-            // an ajax request checks that the form is valid and saves the changes
-            e.preventDefault();
-            var that = $(e.target).parents('#digest-option');
-
-            save_digest_form();
-
-            function save_digest_form() {
-                var csrf_token = document.getElementsByName('csrfmiddlewaretoken')[0].value;
-
-                $.ajax({
-                    data: { csrfmiddlewaretoken: csrf_token, action: 'save',
-                            is_active: $('#id_digest_active').prop('checked')?
-                                                                   'True':'False',
-                            email: $('#id_digest_email').val(),
-                            frequency: $('#id_frequency').val(),
-                            day_of_week: $('#id_day_of_week').val(),
-                            day_of_month: $('#id_day_of_month').val(),
-                            send_if_none: $('#id_send_if_none').prop('checked')?
-                                                                     'True':'False' },
-                    type: 'POST',
-                    url: 'save-digest/',
-                    success: function(data) {
-                        if (data == '') {
-                            form_status('Saved!');
-                        } else {
-                            form_status('Something went wrong');
-                        }
-                        add_errors(data);
-                    }
-                });
-
-                function form_status(status) {
-                    var delay = 5000;
-                    var timer;
-                    clearTimeout(timer);
-                    that.find('#saved').addClass('label label-info');
-                    $('#saved').text(status);
-                    // Fade in 600ms, wait 5s, fade out 600ms
-                    $('#saved').fadeIn('slow');
-                    timer = setTimeout('$("#saved").fadeOut("slow")', delay);
-                }
-            }
-        },
+        save_digest_form(form);
     });
-
-    var Search = new SearchView;
 
     date_select();
 
@@ -70,6 +20,43 @@ $(function() {
         }
     });
 });
+
+function save_digest_form(form) {
+    var csrf_token = document.getElementsByName('csrfmiddlewaretoken')[0].value;
+
+    $.ajax({
+        data: { csrfmiddlewaretoken: csrf_token, action: 'save',
+                is_active: $('#id_digest_active').prop('checked')?
+                                                       'True':'False',
+                email: $('#id_digest_email').val(),
+                frequency: $('#id_frequency').val(),
+                day_of_week: $('#id_day_of_week').val(),
+                day_of_month: $('#id_day_of_month').val(),
+                send_if_none: $('#id_send_if_none').prop('checked')?
+                                                         'True':'False' },
+        type: 'POST',
+        url: 'save-digest/',
+        success: function(data) {
+            if (data == '') {
+                form_status('Saved!', form);
+            } else {
+                form_status('Something went wrong', form);
+            }
+            add_errors(data);
+        }
+    });
+}
+
+function form_status(status, form) {
+    var delay = 5000;
+    var timer;
+    clearTimeout(timer);
+    form.find('#saved').addClass('label label-info');
+    $('#saved').text(status);
+    // Fade in 600ms, wait 5s, fade out 600ms
+    $('#saved').fadeIn('slow');
+    timer = setTimeout('$("#saved").fadeOut("slow")', delay);
+}
 
 function add_errors(data) {
     // remove color from labels of current errors
