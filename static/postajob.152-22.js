a53b965a4ea0e39650c1ee764772e753a899152c
almathew <ashley@directemployersfoundation.org>
2014-05-21 15:48:06 -0400

    Clear inputs after they've been switched out

--- static/postajob.152-22.js
+++ static/postajob.152-22.js
@@ -16,21 +16,31 @@ function show_apply_field(field_name) {
     $('.' + field_name + '-field').show();
 }
 
+function clear_input(field_name) {
+    $('#id_' + field_name).val('');
+}
+
 function update_apply_fields() {
     apply_type = $('#id_apply_type').val();
 
     if(apply_type == 'link') {
         show_apply_field('apply-link');
+        clear_input('apply_email');
+        clear_input('apply_info');
         hide_apply_field('apply-email');
         hide_apply_field('apply-instructions')
     }
     else if(apply_type == 'email') {
         show_apply_field('apply-email');
+        clear_input('apply_email');
+        clear_input('apply_link');
         hide_apply_field('apply-link');
         hide_apply_field('apply-instructions')
     }
     else {
         show_apply_field('apply-instructions');
+        clear_input('apply_email');
+        clear_input('apply_link');
         hide_apply_field('apply-link');
         hide_apply_field('apply-email');
     }
