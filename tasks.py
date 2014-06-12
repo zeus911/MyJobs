d8a66c21e0f8cb3a8b354c9981affdc921e3470f
tdphillips <troy@directemployersfoundation.org>
2014-06-12 09:20:40 -0400

    Improve finding of company_id

--- tasks.py
+++ tasks.py
@@ -15,8 +15,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.core import mail
 from django.template.loader import render_to_string
 from django.db.models import Q
-from mydashboard.models import Company
 
+from mydashboard.models import BusinessUnit, Company, SeoSite
 from myjobs.models import EmailLog, User
 from myprofile.models import SecondaryEmail
 from mysearches.models import SavedSearch, SavedSearchDigest
@@ -364,24 +364,49 @@ def parse_log(logs, solr_location):
                             update_dict.update(object_to_dict(User, user))
 
                     buid = update_dict['job_view_buid']
-                    if buid not in log_memo:  # We haven't seen this buid before
-                        try:
-                            # See if there is a company associated with it
-                            company_id = Company.objects.get(
-                                job_source_ids=buid).pk
-                        except Company.DoesNotExist:
-                            # There is not; default to DirectEmployers
-                            # Association
-                            company_id = 999999
+                    domain = update_dict.get('domain', None)
+                    if not any([buid in log_memo,
+                                domain in log_memo]):
+                        # We haven't seen this buid or domain before
+                        if buid == '0' and domain is not None:
+                            # Retrieve company id via domain
+                            try:
+                                site = SeoSite.objects.get(domain=domain)
+                                company_id = site.business_units.values_list(
+                                    'company__pk', flat=True)[0]
+                            except (SeoSite.DoesNotExist,
+                                    IndexError):
+                                # SeoSite.DoesNotExist: Site does not exist
+                                #   with the given domain
+                                # IndexError: SeoSite exists, but is not
+                                #   associated with business units or companies
+                                company_id = 999999
+                            key = domain
+                        else:
+                            # Retrieve company id via buid
+                            try:
+                                # See if there is a company associated with it
+                                company_id = Company.objects.filter(
+                                    job_source_ids=buid)[0].pk
+                            except IndexError:
+                                # There is not; default to DirectEmployers
+                                # Association
+                                company_id = 999999
+                            key = buid
 
+                        # The defining feature of a given document will either
+                        # be the domain or the buid.
                         # Since buids are being pulled out of query strings as
                         # strings, our memoization dict structure should be
                         # {str(buid): int(company_id)}. Log the new buid
-                        log_memo[buid] = company_id
+                        log_memo[key] = company_id
 
-                    # By this point, we are guaranteed that buid is in log_memo;
-                    # pull the company id from the memo dict
-                    update_dict['company_id'] = log_memo[buid]
+                    # By this point, we are guaranteed that the correct key is
+                    # in log_memo; pull the company id from the memo dict.
+                    if domain is not None and domain in log_memo:
+                        update_dict['company_id'] = log_memo[domain]
+                    else:
+                        update_dict['company_id'] = log_memo[buid]
 
                     update_dict['uid'] = 'analytics##%s#%s' % \
                         (update_dict['view_date'], aguid)
