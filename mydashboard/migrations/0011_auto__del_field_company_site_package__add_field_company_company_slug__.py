2f50b8e6307f79f9923d2c69fe28a8a46e8ae58f
Ashley Mathew <ashley@directemployersfoundation.org>
2014-07-10 14:39:40 -0400

    Option to create and manage your own company

---
+++ mydashboard/migrations/0011_auto__del_field_company_site_package__add_field_company_company_slug__.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Company.company_slug'
+        db.add_column('seo_company', 'company_slug',
+                      self.gf('django.db.models.fields.SlugField')(max_length=200, null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Company.logo_url'
+        db.add_column('seo_company', 'logo_url',
+                      self.gf('django.db.models.fields.URLField')(max_length=200, null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Company.linkedin_id'
+        db.add_column('seo_company', 'linkedin_id',
+                      self.gf('django.db.models.fields.CharField')(max_length=20, null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Company.og_img'
+        db.add_column('seo_company', 'og_img',
+                      self.gf('django.db.models.fields.URLField')(max_length=200, null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Company.canonical_microsite'
+        db.add_column('seo_company', 'canonical_microsite',
+                      self.gf('django.db.models.fields.URLField')(max_length=200, null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Company.digital_strategies_customer'
+        db.add_column('seo_company', 'digital_strategies_customer',
+                      self.gf('django.db.models.fields.BooleanField')(default=False),
+                      keep_default=False)
+
+        # Adding field 'Company.enhanced'
+        db.add_column('seo_company', 'enhanced',
+                      self.gf('django.db.models.fields.BooleanField')(default=False),
+                      keep_default=False)
+
+        # Adding field 'Company.user_created'
+        db.add_column('seo_company', 'user_created',
+                      self.gf('django.db.models.fields.BooleanField')(default=False),
+                      keep_default=False)
+
+
+        # Changing field 'Company.id'
+        db.alter_column('seo_company', u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True))
+
+    def backwards(self, orm):
+        # Deleting field 'Company.company_slug'
+        db.delete_column('seo_company', 'company_slug')
+
+        # Deleting field 'Company.logo_url'
+        db.delete_column('seo_company', 'logo_url')
+
+        # Deleting field 'Company.linkedin_id'
+        db.delete_column('seo_company', 'linkedin_id')
+
+        # Deleting field 'Company.og_img'
+        db.delete_column('seo_company', 'og_img')
+
+        # Deleting field 'Company.canonical_microsite'
+        db.delete_column('seo_company', 'canonical_microsite')
+
+        # Deleting field 'Company.digital_strategies_customer'
+        db.delete_column('seo_company', 'digital_strategies_customer')
+
+        # Deleting field 'Company.enhanced'
+        db.delete_column('seo_company', 'enhanced')
+
+        # Deleting field 'Company.user_created'
+        db.delete_column('seo_company', 'user_created')
+
+        # Changing field 'Company.id'
+        db.alter_column('seo_company', 'id', self.gf('django.db.models.fields.IntegerField')(primary_key=True))
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'mydashboard.businessunit': {
+            'Meta': {'object_name': 'BusinessUnit', 'db_table': "'seo_businessunit'"},
+            'id': ('django.db.models.fields.IntegerField', [], {'max_length': '10', 'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True', 'blank': 'True'})
+        },
+        u'mydashboard.company': {
+            'Meta': {'object_name': 'Company', 'db_table': "'seo_company'"},
+            'canonical_microsite': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'company_slug': ('django.db.models.fields.SlugField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'digital_strategies_customer': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'enhanced': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'job_source_ids': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['mydashboard.BusinessUnit']", 'symmetrical': 'False'}),
+            'linkedin_id': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'logo_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'member': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '200'}),
+            'og_img': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'prm_access': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'product_access': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'user_created': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        u'mydashboard.companyuser': {
+            'Meta': {'unique_together': "(('user', 'company'),)", 'object_name': 'CompanyUser'},
+            'company': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['mydashboard.Company']"}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'group': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['myjobs.User']"})
+        },
+        u'mydashboard.seosite': {
+            'Meta': {'ordering': "(u'domain',)", 'object_name': 'SeoSite', 'db_table': "'seo_seosite'", '_ormbases': [u'sites.Site']},
+            'business_units': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['mydashboard.BusinessUnit']", 'null': 'True', 'blank': 'True'}),
+            'site_package': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['postajob.SitePackage']", 'null': 'True', 'on_delete': 'models.SET_NULL'}),
+            u'site_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['sites.Site']", 'unique': 'True', 'primary_key': 'True'}),
+            'site_title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '200', 'blank': 'True'}),
+            'view_sources': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['mydashboard.ViewSource']", 'null': 'True', 'blank': 'True'})
+        },
+        u'mydashboard.viewsource': {
+            'Meta': {'object_name': 'ViewSource', 'db_table': "'seo_viewsource'"},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '200'}),
+            'view_source': ('django.db.models.fields.IntegerField', [], {'default': "''", 'max_length': '20'})
+        },
+        u'myjobs.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '255', 'db_index': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'gravatar': ('django.db.models.fields.EmailField', [], {'db_index': 'True', 'max_length': '255', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_disabled': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'last_response': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'opt_in_employers': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'opt_in_myjobs': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'password_change': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'profile_completion': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'source': ('django.db.models.fields.CharField', [], {'default': "'https://secure.my.jobs'", 'max_length': '255'}),
+            'timezone': ('django.db.models.fields.CharField', [], {'default': "'America/New_York'", 'max_length': '255'}),
+            'user_guid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100', 'db_index': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"})
+        },
+        u'postajob.package': {
+            'Meta': {'object_name': 'Package'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'postajob.sitepackage': {
+            'Meta': {'object_name': 'SitePackage', '_ormbases': [u'postajob.Package']},
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['mydashboard.Company']", 'null': 'True', 'blank': 'True'}),
+            u'package_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['postajob.Package']", 'unique': 'True', 'primary_key': 'True'}),
+            'sites': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['mydashboard.SeoSite']", 'null': 'True', 'symmetrical': 'False'})
+        },
+        u'sites.site': {
+            'Meta': {'ordering': "(u'domain',)", 'object_name': 'Site', 'db_table': "u'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['mydashboard']
--- mydashboard/models.py
+++ mydashboard/models.py
@@ -9,33 +9,70 @@ from postajob.models import SitePackage
 
 class Company(models.Model):
     """
-    Companies are the central hub for a group of modules and employers.
-
-    *** Why isn't the primary key an auto incrementing field? ***
-    To keep the my.jobs Company model in sync with the microsites Company
-    model the id is the same to ease the transition to a single model down the
-    line.
+    This model defines companies that come from various job sources (currently
+    business units).
 
     """
-    id = models.IntegerField(primary_key=True)
+    def __unicode__(self):
+        return "%s" % self.name
+
+    class Meta:
+        verbose_name = 'Company'
+        verbose_name_plural = 'Companies'
+        ordering = ['name']
+        db_table = 'seo_company'
+
+    def save(self, *args, **kwargs):
+        self.company_slug = slugify(self.name)
+        super(Company, self).save(*args, **kwargs)
+
+    def associated_jobs(self):
+        b_units = self.job_source_ids.all()
+        job_count = 0
+        for unit in b_units:
+            job_count += unit.associated_jobs
+        return job_count
+
+    def featured_on(self):
+        return ", ".join(self.seosite_set.all().values_list("domain",
+                                                            flat=True))
     admins = models.ManyToManyField(User, through='CompanyUser')
-    name = models.CharField('Name', max_length=200, unique=True)
-    job_source_ids = models.ManyToManyField('BusinessUnit')
-    member = models.BooleanField('DirectEmployers Association Member',
-                                 default=False)
     site_package = models.ForeignKey(SitePackage, null=True,
                                      on_delete=models.SET_NULL)
+    name = models.CharField('Name', max_length=200, unique=True)
+    company_slug = models.SlugField('Company Slug', max_length=200, null=True,
+                                    blank=True)
+    job_source_ids = models.ManyToManyField('BusinessUnit')
+    logo_url = models.URLField('Logo URL', max_length=200, null=True,
+                               blank=True, help_text="The url for the 100x50 "
+                                                     "logo image for this "
+                                                     "company.")
+    linkedin_id = models.CharField('LinkedIn Company ID',
+                                   max_length=20, null=True, blank=True,
+                                   help_text="The LinkedIn issued company "
+                                             "ID for this company.")
+    og_img = models.URLField('Open Graph Image URL', max_length=200, null=True,
+                             blank=True, help_text="The url for the large "
+                                                   "format logo for use when "
+                                                   "sharing jobs on "
+                                                   "LinkedIn, and other social "
+                                                   "platforms that support"
+                                                   " OpenGraph.")
+    canonical_microsite = models.URLField('Canonical Microsite URL',
+                                          max_length=200, null=True, blank=True,
+                                          help_text="The primary "
+                                                    "directemployers microsite "
+                                                    "for this company.")
+    member = models.BooleanField('DirectEmployers Association Member',
+                                 default=False)
+    digital_strategies_customer = models.BooleanField('Digital Strategies '
+                                                      'Customer', default=False)
+    enhanced = models.BooleanField('Enhanced', default=False)
 
     # Permissions
     prm_access = models.BooleanField(default=True)
     product_access = models.BooleanField(default=False)
-
-    def __unicode__(self):
-        return self.name
-
-    class Meta:
-        db_table = 'seo_company'
-        verbose_name_plural = 'companies'
+    user_created = models.BooleanField(default=False)
 
     def slugified_name(self):
         return slugify(self.name)
@@ -121,6 +158,7 @@ class BusinessUnit(models.Model):
 
 class CompanyUser(models.Model):
     GROUP_NAME = 'Employer'
+    ADMIN_GROUP_NAME = 'Purchased Microsite Admin'
 
     user = models.ForeignKey(User)
     company = models.ForeignKey(Company)
@@ -144,3 +182,9 @@ class CompanyUser(models.Model):
 
     class Meta:
         unique_together = ('user', 'company')
+
+    def make_purchased_microsite_admin(self):
+        group, _ = Group.objects.get_or_create(name=self.ADMIN_GROUP_NAME)
+        print group
+        self.group.add(group)
+        self.save()
