49593204544f180240737f39a4acd024e9fecc94
aspidites <emarshall85@gmail.com>
2014-08-06 15:17:42 -0400

    Made a copy of the interests list.

--- mypartners/helpers.py
+++ mypartners/helpers.py
@@ -376,7 +376,7 @@ def get_partners_from_filters(request, company, partner_library=False):
     keywords = request.REQUEST.get('keywords', "").split(',', 1)
     city = request.REQUEST.get('city', "").strip()
     state = request.REQUEST.get('state', "").strip()
-    special_interest = request.REQUEST.getlist('special_interest')
+    special_interest = request.REQUEST.getlist('special_interest')[:]
 
 
     # The starting QuerySet will be different if we are searching through the
