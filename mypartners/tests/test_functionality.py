ce0429aed6a7f5859be6eadd85bd6669ebe51b11
aspidites <emarshall85@gmail.com>
2014-08-06 11:18:39 -0400

    Started rewriting selenium tests. Changed duplicate filtering strategy for partner library import.

--- mypartners/tests/test_functionality.py
+++ mypartners/tests/test_functionality.py
@@ -1,27 +1,15 @@
-from django.test import TestCase
+""" Functional Test for MyPartners using Selenium """
+
+from django_liveserver.textcases import LiveServerTextCase
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
+class SeleniumTestCase(LiveServerTestCase):
 
-class TestNewAccount(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = driver.PhantomJS()
+        super(SeleniumTestCase, cls).setUpClass()
 
-    def setUp(self):
-        self.driver = webdriver.PhantomJS()
-        self.email = 'foo@bar.com'
-        self.password = 'password'
-
-        self.driver.get('http://localhost:8000')
-
-    def tearDown(self):
-        self.driver.quit()
-
-    def test_new_user(self):
-        email = self.driver.find_element_by_name('email')
-        password1 = self.driver.find_element_by_name('password1')
-        password2 = self.driver.find_element_by_name('password2')
-        register = self.driver.find_element_by_name('register')
-
-        email.send_keys(self.email)
-        password1.send_keys(self.password)
-        password2.send_keys(self.password)
-        register.click()
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
