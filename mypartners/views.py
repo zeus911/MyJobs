6316dad6f2cc07bbc6ab2b6fc27d6cef09194324
aspidites <emarshall85@gmail.com>
2014-08-20 16:06:10 -0400

    Used urlparse to split url instead of plain string parsing

--- mypartners/views.py
+++ mypartners/views.py
@@ -11,6 +11,7 @@ import newrelic.agent
 import pytz
 import re
 import sys
+from urlparse import urlparse
 
 from django.conf import settings
 from django.contrib.auth.decorators import user_passes_test
@@ -399,7 +400,7 @@ def prm_edit_saved_search(request):
     else:
         form = PartnerSavedSearchForm(partner=partner)
 
-    microsites = {site.lstrip('https://').lower()
+    microsites = {urlparse(site).netloc.lower()
                   for site in get_company_microsites(company)[0]}
 
     content_type = ContentType.objects.get_for_model(PartnerSavedSearch)
