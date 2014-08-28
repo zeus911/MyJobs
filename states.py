6d11440ffc447b95f4f75eae0eb61a850ff4891b
aspidites <emarshall85@gmail.com>
2014-08-28 08:18:27 -0400

    using map instead of choices, since choices doesn't provide abbreviations.

--- states.py
+++ states.py
@@ -4,5 +4,5 @@ from postajob.models import Job
 # example, synonyms['IN'] and synonyms['Indiana'] both return ['IN',
 # 'Indiana'].
 synonyms = dict(
-    [(key, [key, value]) for key, value in Job.get_state_choices()] +
-    [(value, [key, value]) for key, value in Job.get_state_choices()])
+    [(key, [key, value]) for key, value in Job.get_state_map().items()] +
+    [(value, [key, value]) for key, value in Job.get_state_map().items()])
