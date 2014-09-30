a2c7c81b97ba0e80f522ab4cc321d8b31fb4a008
aspidites <emarshall85@gmail.com>
2014-09-30 14:57:41 -0400

    [PD-338] Removed address fields

--- mypartners/models.py
+++ mypartners/models.py
@@ -73,19 +73,6 @@ class Contact(models.Model):
     phone = models.CharField(max_length=30, verbose_name='Phone', blank=True)
     label = models.CharField(max_length=60, verbose_name='Address Label',
                              blank=True)
-    address_line_one = models.CharField(max_length=255,
-                                        verbose_name='Address Line One',
-                                        blank=True)
-    address_line_two = models.CharField(max_length=255,
-                                        verbose_name='Address Line Two',
-                                        blank=True)
-    city = models.CharField(max_length=255, verbose_name='City', blank=True)
-    state = models.CharField(max_length=5, verbose_name='State/Region',
-                             blank=True)
-    country_code = models.CharField(max_length=3, verbose_name='Country',
-                                    blank=True)
-    postal_code = models.CharField(max_length=12, verbose_name='Postal Code',
-                                   blank=True)
     locations = models.ManyToManyField('Location', related_name='contacts')
     tags = models.ManyToManyField('Tag', null=True)
     notes = models.TextField(max_length=1000, verbose_name='Notes', blank=True)
