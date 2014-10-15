c4ec2105d08e3ade0b2d31323090350014256356
aspidites <emarshall85@gmail.com>
2014-10-15 09:48:03 -0400

    Made version.py accessible as a runnable script

--- version.py
+++ version.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+
 """
 Version.py - used to track the version number and build number for the 
 Microsite platform. This file is loaded where the version number is needed.
@@ -19,3 +21,26 @@ month_delta = ((now.year-(start.year+1))*12)+(12-start.month+1)+now.month
 build_calculated = "%s-%s" % (month_delta,now.day)
 # generate the full release number (ie 1.0.001-01)
 release_number = "%s.%s" % (marketing_version,build_calculated)
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description='Get version information in different formats.')
+    parser.add_argument(
+        '-b', '--build-calculated',
+        action='store_true',
+        help='Print the calculated build number, often used to name static '
+             'files.')
+    parser.add_argument(
+        '-r', '--release-number',
+        action='store_true',
+        help='Print the current release number')
+
+    args = parser.parse_args()
+
+    if args.build_calculated:
+        print build_calculated
+
+    if args.release_number:
+        print release_number
