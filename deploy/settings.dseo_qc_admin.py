c459fe3cea0cc3a58cc11845689946467138f9b4
Matt Lauber <github@mklauber.com>
2014-11-10 15:43:08 -0500

    Pep8

--- deploy/settings.dseo_qc_admin.py
+++ deploy/settings.dseo_qc_admin.py
@@ -57,7 +57,7 @@ HAYSTACK_CONNECTIONS = {
 
 ROOT_URLCONF = 'dseo_urls'
 MIDDLEWARE_CLASSES += ('wildcard.middleware.WildcardMiddleware', )
-WILDCARD_REDIRECT=False
+WILDCARD_REDIRECT = False
 TEMPLATE_CONTEXT_PROCESSORS += (
     "social_links.context_processors.social_links_context",
     "seo.context_processors.site_config_context",
