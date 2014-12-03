01aa5efb5e15a85bd764beea26647d8b1da2cdd6
aspidites <emarshall85@gmail.com>
2014-12-03 10:59:53 -0500

    [PD-844] Added authorize.net link to company profile.

--- postajob/models.py
+++ postajob/models.py
@@ -723,7 +723,10 @@ class CompanyProfile(models.Model):
 
     # Only used for Partner Microsites.
     authorize_net_login = models.CharField(
-        max_length=255, blank=True, verbose_name="Authorize.net Login")
+        max_length=255, blank=True, verbose_name="Authorize.net Login",
+        help_text="<a href='https://www.authorize.net' target='blank_'>"
+                  "Create Authorize.net account"
+                  "</a>")
     authorize_net_transaction_key = models.CharField(
         max_length=255, blank=True,
         verbose_name="Authorize.net Transaction Key"
