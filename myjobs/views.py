e44f844d9b61ba642ff708d3126f04f6a4aa8fe9
David Galitsky <galitskyd@gmail.com>
2015-01-20 16:19:04 -0500

    pep8

--- myjobs/views.py
+++ myjobs/views.py
@@ -420,7 +420,8 @@ def batch_message_digest(request):
                         for event_str in events:
                             if event_str == '':
                                 continue
-                            try:  # nested try :/ -need to catch json exceptions
+                            # nested try :/ -need to catch json exceptions
+                            try:
                                 event_list.append(json.loads(event_str))
                             except ValueError, e:  # return 404 is bad json
                                 return HttpResponse(status=400)
