5be4cf37dd86abd1afcd71b44651d8bff44a5bad
aspidites <emarshall85@gmail.com>
2015-01-22 13:56:13 -0500

    Made decorator more specific

--- myreports/views.py
+++ myreports/views.py
@@ -2,14 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from myreports.decorators import restrict_to_staff_when
+from myreports.decorators import restrict_to_staff
 
 
-@restrict_to_staff_when(lambda r: not r.user.is_staff)
+@restrict_to_staff()
 def reports_overview(request):
     return render_to_response('myreports/reports_overview.html', {},
                               RequestContext(request))
 
-@restrict_to_staff_when(lambda r: not r.user.is_staff)
+@restrict_to_staff()
 def edit_report(request):
     return HttpResponse()
