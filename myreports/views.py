ccc7aefcf271b66d984cb98858e76d44ec984308
aspidites <emarshall85@gmail.com>
2015-01-22 11:49:35 -0500

    Made stub links work. Renamed view to be consistent with other apps.

--- myreports/views.py
+++ myreports/views.py
@@ -1,10 +1,14 @@
-from django.http import Http404
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 
+@user_passes_test(lambda u: u.is_staff)
 def reports_overview(request):
-    if not request.user.is_staff:
-        raise Http404
     return render_to_response('myreports/reports_overview.html', {},
                               RequestContext(request))
+
+@user_passes_test(lambda u: u.is_staff)
+def edit_report(request):
+    return HttpResponse()
