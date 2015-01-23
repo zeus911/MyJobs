945dbcc20eeba128d1c96af3782f91f16cdd3f5c
David Galitsky <galitskyd@gmail.com>
2015-01-23 15:55:22 -0500

    removed reports_overview

--- myreports/views.py
+++ myreports/views.py
@@ -6,11 +6,6 @@ from myreports.decorators import restrict_to_staff
 
 
 @restrict_to_staff()
-def reports_overview(request):
-    return render_to_response('myreports/reports_overview.html', {},
-                              RequestContext(request))
-
-@restrict_to_staff()
-def create_report(request):
-    return render_to_response('myreports/create_report.html', {},
+def reports(request):
+    return render_to_response('myreports/reports.html', {},
                               RequestContext(request))
