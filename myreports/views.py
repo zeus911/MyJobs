498dc6163b592a5632c41bcc7880192f59bfa19c
David Galitsky <galitskyd@gmail.com>
2015-01-27 13:15:36 -0500

    modified view to handle ajax

--- myreports/views.py
+++ myreports/views.py
@@ -7,5 +7,13 @@ from myreports.decorators import restrict_to_staff
 
 @restrict_to_staff()
 def reports(request):
+    if request.is_ajax():
+        response = HttpResponse()
+        template = 'myreports/prm/page{page}.html'
+        html = render_to_response(template.format(page=request.GET['page']),
+                                  {}, RequestContext(request))
+        response.content = html.content
+        return response
+
     return render_to_response('myreports/reports.html', {},
                               RequestContext(request))
