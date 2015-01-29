b4c7545ea880183ce2d2210daf8baee9c941fc83
aspidites <emarshall85@gmail.com>
2015-01-29 17:33:51 -0500

    Reverted User factory for now.

--- myjobs/tests/factories.py
+++ myjobs/tests/factories.py
@@ -3,16 +3,20 @@ from myjobs.models import *
 
 
 class UserFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = 'myjobs.User'
-
-    email = factory.Sequence(lambda n: 'alice%i@example.com' % n)
+    FACTORY_FOR = User
+    email = 'alice@example.com'
     gravatar = 'alice@example.com'
     password = '5UuYquA@'
     user_guid = factory.LazyAttribute(lambda n: '{0}'.format(uuid.uuid4()))
     is_active = True
     is_verified = True
 
-    @factory.post_generation
-    def password(obj, create, extracted, **kwargs):
-        obj.set_password(create)
+    @classmethod
+    def _prepare(cls, create, **kwargs):
+        password = kwargs.pop('password', None)
+        user = super(UserFactory, cls)._prepare(create, **kwargs)
+        if password:
+            user.set_password(password)
+            if create:
+                user.save()
+        return user
