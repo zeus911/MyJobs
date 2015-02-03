f4b682602c8207a86f3ca6795e12a8483d440ce4
tdphillips <troy@directemployersfoundation.org>
2015-02-03 10:29:36 -0500

    Allow deleting messages [PD-956]

--- mymessages/views.py
+++ mymessages/views.py
@@ -1,8 +1,11 @@
+import json
+
 from django.contrib.auth.decorators import user_passes_test
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404, HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.template.loader import render_to_string
 
 from myjobs.decorators import user_is_allowed
 from myjobs.models import User
@@ -20,9 +23,7 @@ def read(request):
     raise Http404
 
 
-@user_is_allowed()
-@user_passes_test(User.objects.not_disabled)
-def inbox(request):
+def get_message_page(request):
     message_list = request.user.messages(only_new=False)
     paginator = Paginator(message_list, 10)
     page = request.GET.get('page')
@@ -32,5 +33,34 @@ def inbox(request):
         messages = paginator.page(1)
     except EmptyPage:
         messages = paginator.page(paginator.num_pages)
+    return messages
+
+
+@user_is_allowed()
+@user_passes_test(User.objects.not_disabled)
+def delete(request):
+    if request.is_ajax():
+        message_id, user = request.GET.get('name').split('-')[2:]
+        try:
+            info = MessageInfo.objects.get(user=user, message__id=message_id)
+        except MessageInfo.DoesNotExist:
+            pass
+        else:
+            if info.message.messageinfo_set.count() > 1:
+                info.delete()
+            else:
+                info.message.delete()
+        messages = get_message_page(request)
+        response = render_to_string('mymessages/includes/messages.html',
+                                    {'messages': messages},
+                                     RequestContext(request))
+        return HttpResponse(json.dumps(response))
+    raise Http404
+
+
+@user_is_allowed()
+@user_passes_test(User.objects.not_disabled)
+def inbox(request):
+    messages = get_message_page(request)
     return render_to_response('mymessages/inbox.html', {'messages': messages},
                               RequestContext(request))
--- static/inbox.160-29.js
+++ static/inbox.160-29.js
@@ -4,17 +4,44 @@ $(document).ready(function() {
         var target = $(e.target);
         if (!target.is('a')) {
             // We don't want to open a modal if the user is clicking on an
-            // anchor that happens to be in this row.
+            // anchor that happens to be in this row (delete, mark read).
             var message_id = target.closest('tr').attr('id'),
                 modal_selector = '#' + message_id + '-full';
             $(modal_selector).modal();
+            on_read(target);
         }
+    });
+
+    $('[class*=mymessage-delete-]').click(function(){
+        delete_message(this);
     });
 });
 
-var on_read = function(clicked) {
-    // On future views, this occurs during template processing; As this was
-    // initiated by ajax, that doesn't happen.
-    clicked.hide();
-    clicked.parents('tr').addClass('read');
-};
+function on_read(clicked) {
+    var parent = clicked.parents('tr');
+    // On future views, this is all done at the template level; As this
+    // was initiated by ajax, that doesn't happen.
+    if (clicked.is('a[class*=mymessage-read]')) {
+        clicked.hide();
+    } else {
+        var button = parent.find('a[class*=mymessage-read]');
+        readMessage(button);
+        button.hide();
+    }
+    parent.addClass('read');
+}
+
+function delete_message(button) {
+    var message_box = $(button),
+        name = message_box.attr('class').split(' ').pop(),
+        data = "name="+name;
+    $.ajax({
+        type: 'GET',
+        url: '/message/delete/',
+        data: data,
+        dataType: 'json',
+        success: function(data) {
+            $('#messages').replaceWith(data);
+        }
+    });
+}
