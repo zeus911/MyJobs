f09877e11ac085cc0c8f58fc3d43789c348c9f34
aspidites <emarshall85@gmail.com>
2015-02-11 15:43:43 -0500

    Removed useless indentation

--- templates/includes/group_perm_handler.js
+++ templates/includes/group_perm_handler.js
@@ -1,19 +1,19 @@
-    //$().on doesn't work with older versions of jQuery, which django uses
-    $('#id_group').change(function(){
-	    var val = $(this).val();
-	    if(val){
-            // set the value from group ddl to GroupPermission.groupId
-            GroupPermission.setGroupId(val);
-	    	// check if we already have this ajax call's output cached and use it
-	        if(GroupPermission.getDataCacheEntry(val)){
-	            GroupPermission.setSelectsByGroup(GroupPermission.getDataCacheEntry(val));
-	        }
-	        // otherwise send ajax call and return output to GroupPermission.setSelectsByGroup
-	        else{
-	            $.getJSON("/admin/groupsites/", {"groupId" : GroupPermission.getGroupId()}, GroupPermission.setSelectsByGroup);
-	        }            
-	    } else{
-	    	// if the group ddl selected the "none" option, hide the site(s) select box(s)
-	        $(GroupPermission.getSitesContainer()).hide();
-	    }
-	});
+//$().on doesn't work with older versions of jQuery, which django uses
+$('#id_group').change(function(){
+    var val = $(this).val();
+    if(val){
+        // set the value from group ddl to GroupPermission.groupId
+        GroupPermission.setGroupId(val);
+        // check if we already have this ajax call's output cached and use it
+        if(GroupPermission.getDataCacheEntry(val)){
+            GroupPermission.setSelectsByGroup(GroupPermission.getDataCacheEntry(val));
+        }
+        // otherwise send ajax call and return output to GroupPermission.setSelectsByGroup
+        else{
+            $.getJSON("/admin/groupsites/", {"groupId" : GroupPermission.getGroupId()}, GroupPermission.setSelectsByGroup);
+        }            
+    } else{
+        // if the group ddl selected the "none" option, hide the site(s) select box(s)
+        $(GroupPermission.getSitesContainer()).hide();
+    }
+});
