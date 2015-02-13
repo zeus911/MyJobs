a9b2d265ef8d32c9377ec63d0898da5d0c3e4df7
Ashley Mathew <ashley@directemployersfoundation.org>
2015-02-13 10:56:35 -0500

    Started rounding out the idea of what scheduling tasks would look like. Rolled action-based events into value-based events.

--- myemails/models.py
+++ myemails/models.py
@@ -3,43 +3,61 @@ from django.db import models
 
 
 class EmailTemplate(models.Model):
+    # header = models.ForeignKey()
+    # footers = models.ForeignKey()
+
     pass
 
 
 class Event(models.Model):
+    COMPARISON_CHOICES = (
+        ('', 'is equal to'),
+        ('__gte', 'is greater than or equal to'),
+        ('__lte', 'is less than or equal to'),
+    )
     email_template = models.ForeignKey('EmailTemplate', null=True)
     is_active = models.BooleanField(default=True)
     owner = models.ForeignKey('seo.Company')
 
+    compare_using = models.CharField(choices=COMPARISON_CHOICES)
+    model = models.ForeignKey(ContentType)
+    field = models.CharField(max_length=255)
+
     class Meta:
         abstract = True
 
-    def send_email(self):
-        return
-
-
-class ActionEvent(Event):
-    pass
-
-
-class CronEvent(Event):
-    pass
-
-    def schedule_task(self):
+    def send_email(self, for_object):
         pass
 
 
-class ValueEvent(Event):
-    trigger_model = models.ForeignKey(ContentType)
-    trigger_field = models.CharField(max_length=255)
-    trigger_value = models.PositiveIntegerField()
+class CronEvent(Event):
+    value = models.TimeField()
 
-    def is_triggered(self, obj):
-        actual_value = getattr(obj, self.trigger_field, None)
-        return actual_value == self.trigger_value
+    def schedule_task(self, for_object):
+        EmailCronTask.objects.create(
+            object_id=for_object.pk,
+            model=ContentType.objects.get_for_model(for_object._meta.model),
+            related_event=self,
+            scheduled_for=self.scheduled_for(for_object),
+        )
+
+    def scheduled_for(self, for_object):
+        """
+        :return: The next valid time this Event would be scheduled for.
+
+        """
+        
+        return None
+
+
+class ValueEvent(Event):
+    value = models.PositiveIntegerField()
 
 
 class EmailCronTask(models.Model):
+    object_id = models.PositiveIntegerField()
+    model = models.ForeignKey(ContentType)
+
     completed_on = models.DateTimeField(blank=True, null=True)
     related_event = models.ForeignKey('EmailCron')
     scheduled_for = models.DateTimeField()
