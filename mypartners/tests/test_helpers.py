f260cb4b4443fa34df6b09e225aabb1c7935119b
aspidites <emarshall85@gmail.com>
2015-02-16 08:07:28 -0500

    And if the correct imports are available...

--- mypartners/tests/test_helpers.py
+++ mypartners/tests/test_helpers.py
@@ -185,6 +185,7 @@ class PartnerLibraryFilterTests(PartnerLibraryTestCase):
             '/prm/view',
             {'city': 'Alberson', 'state': 'New York'})
 
+        from mypartners.models import Partner
         self.fail("partners: {0}\nstatus: {1}".format(
             Partner.objects.all(), response.status_code))
         self.assertEqual(response.status_code, 200)
