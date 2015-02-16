f50d656ab0d48fc28758796e64c73f394ce15ee0
aspidites <emarshall85@gmail.com>
2015-02-16 15:48:04 -0500

    Lets just try changing random things until tests pass, yay

--- mypartners/tests/test_helpers.py
+++ mypartners/tests/test_helpers.py
@@ -183,20 +183,8 @@ class PartnerLibraryFilterTests(PartnerLibraryTestCase):
 
         response = self.client.get(
             '/prm/view',
-            {'company': self.company,
+            {'company': self.company.id,
              'city': 'Alberson', 'state': 'New York'})
-
-        from mypartners.models import Partner
-        self.fail("""\
-            
-            company_name: {}
-            partner_companies: {}
-            partners: {}
-            has_partners: {}
-            """.format(response.context['company_name'],
-                       Partner.objects.values_list('owner__name', flat=True),
-                       response.context['partners'],
-                       response.context['has_partners']))
 
         # check that we in fact return partners whos contact's location is the
         # mangled location above
