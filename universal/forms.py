44f7427d8220c2cffdbbd740599c4b1ce30ac24a
aspidites <emarshall85@gmail.com>
2015-02-19 11:35:37 -0500

    Added data migration and form sanitation.

--- universal/forms.py
+++ universal/forms.py
@@ -8,3 +8,11 @@ class RequestForm(ModelForm):
         self.request = kwargs.pop('request', None)
         self.company = get_company(self.request)
         super(RequestForm, self).__init__(*args, **kwargs)
+
+
+class NormalizedModelForm(ModelForm):
+
+    def clean(self):
+        self.cleaned_data = {key: ' '.join(value.split())
+                             for key, value in self.cleaned_data.items()}
+        return super(NormalizedModelForm, self).clean()
