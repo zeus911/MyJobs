37dd2e8184606287d50f6049cf52327743953df9
aspidites <emarshall85@gmail.com>
2015-03-04 08:38:12 -0500

    Fixed json output. Forgot to add things back to a context.

--- myreports/views.py
+++ myreports/views.py
@@ -18,7 +18,7 @@ def reports(request):
     if request.is_ajax():
         response = HttpResponse()
         template = '{path}.html'
-        html = render_to_response(template.format(path=request.POST['output']),
+        html = render_to_response(template.format(path=request.GET['output']),
                                   {}, RequestContext(request))
         response.content = html.content
         return response
@@ -72,16 +72,16 @@ def filter_records(request,
                 'model': 'partner',
                 'output': 'myreports/example_view.html'}))
     """
-    if request.is_ajax() and request.method == 'POST':
+    if True:#request.is_ajax() and request.method == 'GET':
         company = get_company_or_404(request)
         user = request.user
         path = request.get_full_path()
 
         # get rid of empty params and flatten single-item lists
         params = {}
-        for key in request.POST.keys():
-            value = request.POST.get(key)
-            value_list = request.POST.getlist(key)
+        for key in request.GET.keys():
+            value = request.GET.get(key)
+            value_list = request.GET.getlist(key)
 
             # parsing a list parameter as a regular parameter only captures the
             # last item, so if trying both ways returns the same value, we can
@@ -145,8 +145,8 @@ def filter_records(request,
                 if counts:
                     data[-1]['count'] = counts[record['pk']]
 
-            ctx = json.dumps(data, cls=DjangoJSONEncoder,
-                             separators=(',', ';'))
+            ctx['records'] = data
+            ctx = json.dumps(ctx, cls=DjangoJSONEncoder)
 
             response = HttpResponse(
                 ctx, content_type='application/json; charset=utf-8')
@@ -158,5 +158,5 @@ def filter_records(request,
 
         return response
     else:
-        raise Http404("This view is only reachable via an AJAX POST request")
+        raise Http404("This view is only reachable via an AJAX GET request")
 filter_records.cache = {}
