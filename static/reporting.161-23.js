5195b16b0e80a2f979b1109f3b01ca1f5caff065
David Galitsky <galitskyd@gmail.com>
2015-03-05 14:08:25 -0500

    added review info

--- static/reporting.161-23.js
+++ static/reporting.161-23.js
@@ -7,23 +7,25 @@ window.addEventListener("beforeunload", function(e) {
 });
 */
 
-var Report = function (types) {
+
+var Report = function(types) {
   this.types = types;
   this.data = {};
   this.fields = this.create_fields(types);
 };
 
-Report.prototype.create_fields = function (types) {
-   var reports = {"prm": [new Field("Select Date", "date"),
-                          new Field("State", "text"),
-                          new Field("City", "text"),
-                          new List("Select Partners", "partner"),
-                          new List("Select Contacts", "contact")],
-              "compliance": [new Field("test", "text"),
-                             new Field("test2", "text")]
-     },
-     fields = [],
-     key;
+
+Report.prototype.create_fields = function(types) {
+   var reports = {"prm":        [new Field("Select Date", "date"),
+                                 new Field("State", "text"),
+                                 new Field("City", "text"),
+                                 new List("Select Partners", "partner"),
+                                 new List("Select Contacts", "contact")],
+                  "compliance": [new Field("test", "text"),
+                                 new Field("test2", "text")]},
+        fields = [],
+        key;
+
   for (key in types) {
     if (reports.hasOwnProperty(types[key])) {
       fields.push.apply(fields, reports[types[key]]);
@@ -33,16 +35,18 @@ Report.prototype.create_fields = function (types) {
 
 };
 
-Report.prototype.bind_events = function () {
+
+Report.prototype.bind_events = function() {
   var report = this;
 
+
   // Update Partner and Contact Lists
-  $(document.body).on("change", "input:not([id$=-all-checkbox])", function (e) {
+  $(document.body).on("change", "input:not([id$=-all-checkbox])", function(e) {
     console.log("CHANGE!");
 
     var in_list = $(this).parents(".list-body").attr("id"),
-      contact_wrapper = $("#contact-wrapper"),
-      c_field = report.find_field("Select Contacts");
+        contact_wrapper = $("#contact-wrapper"),
+        c_field = report.find_field("Select Contacts");
 
     if (typeof in_list !== "undefined") {
       var all_records = $(this).parents(".list-body").prev().children("input"),
@@ -54,7 +58,7 @@ Report.prototype.bind_events = function () {
       } else {
         // iterate through all checkboxes and find all the ones that are checked
         // and add to data.
-        $(records).each(function (element) {
+        $(records).each(function(element) {
           if ($(records[element]).is(":checked")) {
             values.push(records[element].value);
           }
@@ -63,17 +67,22 @@ Report.prototype.bind_events = function () {
         if (values.length > 0) {
           report.data[in_list] = values;
         } else {
-          report.data[in_list] = "";
+          // Suppose to return 0 contacts
+          // List filter uses IDs which no object will ever have id=0
+          report.data[in_list] = "0";
         }
       }
 
       if (in_list === "partner") {
+        if (typeof report.data.contact !== "undefined") {
+          delete report.data.contact;
+        }
         contact_wrapper.html(c_field.render(report.data)).children().unwrap();
       }
     } else {
       var partner_wrapper = $("#partner-wrapper"),
         p_field = report.find_field("Select Partners"),
-        is_prm_field = function (e) {
+        is_prm_field = function(e) {
         // This list will need to be updated if more is added to the PRM report
         // if they filter down partners/contacts
         var prm_field = ["start_date", "end_date", "state", "city"],
@@ -86,13 +95,20 @@ Report.prototype.bind_events = function () {
       report.data[$(e.currentTarget).attr("id")] = $(e.currentTarget).val();
 
       if (is_prm_field(e)) {
+        if (typeof report.data.partner !== "undefined") {
+          delete report.data.partner;
+        }
+        if (typeof report.data.contact !== "undefined") {
+          delete report.data.contact;
+        }
         partner_wrapper.html(p_field.render(report.data)).children().unwrap();
         contact_wrapper.html(c_field.render(report.data)).children().unwrap();
       }
     }
   });
 
-  $(document.body).on("click", ".list-header", function () {
+
+  $(document.body).on("click", ".list-header", function() {
     var icon = $(this).children("i");
 
     if(icon.hasClass("fa-plus-square-o")) {
@@ -104,14 +120,14 @@ Report.prototype.bind_events = function () {
     }
   });
 
-  // TODO: FIX CLICK BUG! .click has different functionality vs default click on checkboxes.
-  // TODO: so hacked an invisible box over checkboxes for at least consistent behavior
-  $(document.body).on("click", ".list-body li", function () {
+
+  $(document.body).on("click", ".list-body li", function() {
     $(this).children("input").click();
   });
 
+
   // The event that a checked checkbox is clicked (will uncheck the checkbox)
-  $(document.body).on("click", ".list-body input[type='checkbox']:checked", function (e) {
+  $(document.body).on("click", ".list-body input[type='checkbox']:checked", function(e) {
     e.stopPropagation();
     var all_checkbox = $(this).parents("div.list-body").prev().children("input");
     if (all_checkbox.is(":checked")) {
@@ -119,13 +135,15 @@ Report.prototype.bind_events = function () {
     }
   });
 
-  $(document.body).on("click", ".list-body input[type='checkbox']:not(:checked)", function (e) {
+
+  $(document.body).on("click", ".list-body input[type='checkbox']:not(:checked)", function(e) {
     e.stopPropagation();
     var all_checkbox = $(this).parents("div.list-body").prev().children("input"),
-      checkboxes = $(this).parents(".list-body").find("input"),
-      all_are_checked = true,
-      that = this;
-    $(checkboxes).each(function (element) {
+        checkboxes = $(this).parents(".list-body").find("input"),
+        all_are_checked = true,
+        that = this;
+
+    $(checkboxes).each(function(element) {
       // Check to see if all checkboxes are checked
       if (!$(checkboxes[element]).is(":checked") && checkboxes[element] !== that) {
         all_are_checked = false;
@@ -133,6 +151,7 @@ Report.prototype.bind_events = function () {
         return false;
       }
     });
+
     if (!all_are_checked) {
       all_checkbox.prop("checked", false);
     } else {
@@ -140,52 +159,100 @@ Report.prototype.bind_events = function () {
     }
   });
 
-  $(document.body).on("click", "input[id$=-all-checkbox]:checked", function (e) {
+
+  $(document.body).on("click", "input[id$=-all-checkbox]:checked", function(e) {
     e.stopPropagation();
     var checkboxes = $(this).parent().next().find("input");
-    $(checkboxes).each(function (element) {
+
+    $(checkboxes).each(function(element) {
       $(checkboxes[element]).prop("checked", true);
     });
   });
 
-  $(document.body).on("click", "input[id$=-all-checkbox]:not(:checked)", function (e) {
+
+  $(document.body).on("click", "input[id$=-all-checkbox]:not(:checked)", function(e) {
     e.stopPropagation();
     var checkboxes = $(this).parent().next().find("input");
-    $(checkboxes).each(function (element) {
+
+    $(checkboxes).each(function(element) {
       $(checkboxes[element]).prop("checked", false);
     });
   });
 
-  $(document.body).on("click", "#show-modal", function (e) {
+
+  $(document.body).on("click", "#show-modal", function(e) {
     var modal = $("#report-modal"),
-      body = modal.children(".modal-body"),
-      footer = modal.children(".modal-footer");
-    console.log(report.data);
+        body = modal.children(".modal-body"),
+        footer = modal.children(".modal-footer");
     body.html(report.readable_data());
     modal.modal("show");
   });
 
-  $(document.body).on("click", "#gen-report", function (e) {
 
+  $(document.body).on("click", "#gen-report", function(e) {
+    var data = {"csrfmiddlewaretoken": read_cookie("csrftoken")},
+        url = location.protocol + "//" + location.host + "/reports/ajax/mypartners/contactrecord";
+    if (report.data) {
+      $.extend(data, report.data);
+    }
+    console.log(url);
+    $.ajaxSettings.traditional = true;
+    $.ajax({
+      type: 'POST',
+      url: url,
+      data: $.param(data, true),
+      dataType: "json",
+      global: false,
+      success: function (data) {
+        $(".modal-body").html(data);
+      }
+    });
   });
 };
 
-Report.prototype.readable_data = function () {
+
+Report.prototype.readable_data = function() {
   var data = this.data,
-    html = '';
+      html = '',
+      key;
+
+  for (key in data) {
+    if (data.hasOwnProperty(key)) {
+      var value = data[key];
+
+      key = key.replace(/_/g, " ");
+      html += "<label>" + key + ":</label>";
+
+      if (typeof value === "object") {
+        var ul = $("<ul></ul>");
+
+        for (var i = 0; i < value.length; i++) {
+          var name = $("#" + key + " input[value='" + value[i] + "'").next("span").html(),
+              li = $("<li>" + name + "</li>");
+          ul.append(li);
+        }
+
+        html += ul.prop("outerHTML");
+      } else {
+        html += value;
+      }
+    }
+  }
   return html;
 };
 
-Report.prototype.find_field = function (field_label) {
+
+Report.prototype.find_field = function(field_label) {
   return ($.grep(this.fields, function(field) {
     return field.label === field_label;
   })[0]);
 };
 
-Report.prototype.render_fields = function (fields) {
+
+Report.prototype.render_fields = function(fields) {
   var container = $("#container"),
-    html = '',
-    i = 0;
+      html = '',
+      i = 0;
   for (i; i < fields.length; i++) {
     var field = fields[i];
     html += field.render();
@@ -194,53 +261,57 @@ Report.prototype.render_fields = function (fields) {
   container.html(html);
 };
 
-Report.prototype.save_data = function () {
-  console.log("Saving data...");
-};
 
-var Field = function (label, type) {
+var Field = function(label, type) {
   this.label = label;
   this.type = type;
 };
 
-Field.prototype.render = function () {
+
+Field.prototype.render = function() {
   var html = '',
-    wrapper = $("<div></div>"), // wrapping div
-    l = $("<label>" + this.label + "</label>"), // label for <input>
-    input;
+      wrapper = $("<div></div>"), // wrapping div
+      l = $("<label>" + this.label + "</label>"), // label for <input>
+      input;
+
   if (this.type === "text") {
     input = $("<input id='" + this.label.toLowerCase().replace(/ /g, "_") + "' type='text' placeholder='"+ this.label +"' />");
     wrapper.append(l).append(input);
-    html = $("<div>").append(wrapper).remove().html();
+    html = wrapper.prop("outerHTML");
   } else if (this.type === "date") {
     var date_widget = $("<div id='date-filter' class='filter-option'></div>").append("<div class='date-picker'></div>"),
-      date_picker = $(date_widget).children("div")
-                      .append("<input id='start_date' class='datepicker picker-left' type='text' placeholder='Start Date' />")
-                      .append("<span id='activity-to-' class='datepicker'>to</span>")
-                      .append("<input id='end_date' class='datepicker picker-right' type='text' placeholder='End Date' />");
+        date_picker = $(date_widget).children("div")
+                        .append("<input id='start_date' class='datepicker picker-left' type='text' placeholder='Start Date' />")
+                        .append("<span id='activity-to-' class='datepicker'>to</span>")
+                        .append("<input id='end_date' class='datepicker picker-right' type='text' placeholder='End Date' />");
     date_widget.append(date_picker);
-    html = $("<div>").prepend(l).append(date_widget).remove().html();
+    html += l.prop("outerHTML");
+    html += date_widget.prop("outerHTML");
   } else if (this.type === "state") {
     // TODO: ajax state dropdown
   }
   return html;
 };
 
-var List = function (label, type) {
+
+var List = function(label, type) {
   Field.call(this, label, type);
 };
 
+
 List.prototype = Object.create(Field.prototype);
 
-List.prototype.render = function (filter) {
+
+List.prototype.render = function(filter) {
   var container = $("<div id='"+ this.type +"-header' class='list-header'></div>"),
-    icon = $("<i class='fa fa-plus-square-o'></i>"),
-    all_checkbox = $("<input id='"+ this.type +"-all-checkbox' type='checkbox' checked />"),
-    record_count,
-    html,
-    body = $("<div id='"+ this.type +"' class='list-body' style='display: none;'></div>"),
-    wrapper = $("<div id='"+ this.type +"-wrapper'></div>"),
-    list = this;
+      icon = $("<i class='fa fa-plus-square-o'></i>"),
+      all_checkbox = $("<input id='"+ this.type +"-all-checkbox' type='checkbox' checked />"),
+      record_count,
+      html,
+      body = $("<div id='"+ this.type +"' class='list-body' style='display: none;'></div>"),
+      wrapper = $("<div id='"+ this.type +"-wrapper'></div>"),
+      list = this;
+
   if (this.type === "contact") {
     record_count = $("<span style='display: none;'>(<span>0</span> Contacts Selected)</span>");
     container.append(icon).append(all_checkbox).append(" All Contacts ").append(record_count);
@@ -251,17 +322,22 @@ List.prototype.render = function (filter) {
     record_count = $("<span style='display: none;'>(<span>0</span> "+ this.type +" Selected)</span>");
     container.append(icon).append(all_checkbox).append(" All " + this.type + " ").append(record_count);
   }
+
   wrapper.append(container).append(body);
-  html = $("<div>").append(wrapper).remove().html();
+  html = wrapper.prop("outerHTML");
+
   (function() {
     list.filter(list.type, filter);
   })();
+
   return html;
 };
 
-List.prototype.filter = function (type, filter) {
+
+List.prototype.filter = function(type, filter) {
+  "use strict";
   var url = location.protocol + "//" + location.host, // https://secure.my.jobs
-    data = {"csrfmiddlewaretoken": read_cookie("csrftoken")};
+      data = {"csrfmiddlewaretoken": read_cookie("csrftoken")};
 
   if (typeof filter !== "undefined") {
     $.extend(data, filter);
@@ -281,13 +357,13 @@ List.prototype.filter = function (type, filter) {
     data: $.param(data, true),
     dataType: "json",
     global: false,
-    success: function (data) {
+    success: function(data) {
       var ul = $("<ul></ul>"),
-        selected = $("[id^='" + type + "-header'] span span");
+          selected = $("[id^='" + type + "-header'] span span");
       for (var i = 0; i < data.records.length; i++) {
         var record = data.records[i],
-          li = $("<li><input type='checkbox' value='"+ record.pk +"' checked /> "+ record.name +"</li>"),
-          invis_box = $("<div class=\"invis-box\"></div>");
+            li = $("<li><input type='checkbox' value='"+ record.pk +"' checked /> <span>"+ record.name +"</span></li>"),
+            invis_box = $("<div class=\"invis-box\"></div>");
         if (type === "partner") {
           li.append("<span class='pull-right'>"+ record.count +"</span>");
         }
@@ -297,7 +373,7 @@ List.prototype.filter = function (type, filter) {
       $("#"+ type + ".list-body").append(ul);
       $(selected).html(data.records.length).parent().show("fast");
     },
-    error: function (e) {
+    error: function(e) {
       // TODO: change when testing is done to something more useful.
       throw "Something horrible happened.";
     }
@@ -305,8 +381,8 @@ List.prototype.filter = function (type, filter) {
 };
 
 
-$(document).ready(function () {
-  $(document.body).on("click", ".datepicker",function (e) {
+$(document).ready(function() {
+  $(document.body).on("click", ".datepicker",function(e) {
    $(this).pickadate({
      format: "mm/dd/yyyy",
      selectYears: true,
@@ -314,19 +390,20 @@ $(document).ready(function () {
      today: false,
      clear: false,
      close: false,
-     onOpen: function () {
+     onOpen: function() {
        if (this.get("id") === "start-date") {
          var end_date = $("#end-date").val();
          this.set("max", new Date(end_date || new Date()));
        } else if (this.get("id") === "end-date") {
          var start_date = $("#start-date").val();
-         this.set("min", new Date(start_date || new Date(0)))
+         this.set("min", new Date(start_date || new Date(0)));
        }
      }
    });
   });
 
-  $(document.body).on("click", "#start-report:not(.disabled)", function (e) {
+
+  $(document.body).on("click", "#start-report:not(.disabled)", function(e) {
     e.preventDefault();
     var choices = $("#choices input[type='checkbox']:checked"),
       types = [],
@@ -342,14 +419,16 @@ $(document).ready(function () {
     report.render_fields(report.fields);
   });
 
-  $(document.body).on("click", "#choices input[type='checkbox']:checked", function () {
+
+  $(document.body).on("click", "#choices input[type='checkbox']:checked", function() {
     var btn = $("#start-report");
     btn.removeClass("disabled");
   });
 
-  $(document.body).on("click", "#choices input[type='checkbox']:not(:checked)", function () {
+
+  $(document.body).on("click", "#choices input[type='checkbox']:not(:checked)", function() {
     var btn = $("#start-report"),
-      checkboxes = $("#choices input[type='checkbox']");
+        checkboxes = $("#choices input[type='checkbox']");
     if (!checkboxes.is(":checked")) {
       btn.addClass("disabled");
     }
