686a631f2f719c162c4d2f2fd5612b237b5c18f2
aspidites <emarshall85@gmail.com>
2015-03-12 13:35:16 -0400

    Added serialization methods to be used in templates or as convenient from view function.

--- myreports/models.py
+++ myreports/models.py
@@ -1,7 +1,9 @@
+import json
 from django.db import models
 
 
 class Report(models.Model):
+    name = models.CharField(max_length=50)
     created_by = models.ForeignKey('myjobs.User')
     owner = models.ForeignKey('seo.Company')
     created_on = models.DateTimeField(auto_now_add=True)
@@ -10,3 +12,15 @@ class Report(models.Model):
     # json encoded string of the params used to filter
     params = models.CharField(max_length=255)
     results = models.FileField(upload_to='reports')
+
+    def __init__(self, *args, **kwargs):
+        super(Report, self).__init__(*args, **kwargs)
+        self._results = self.results.read()
+
+    @property
+    def json(self):
+        return self._results
+
+    @property
+    def python(self):
+        return json.loads(self._results)
