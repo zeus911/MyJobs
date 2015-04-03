6f5b3b383699c388fdf966d324ad453f9f2fe3c2
tdphillips <troy@directemployersfoundation.org>
2015-04-03 14:59:33 -0400

    Tests and work on unsubscriber [ST-1047]

--- mysearches/forms.py
+++ mysearches/forms.py
@@ -13,6 +13,7 @@ from mypartners.forms import PartnerEmailChoices
 from mypartners.models import Contact, ADDITION, CHANGE
 from registration.models import Invitation
 from mypartners.helpers import log_change, tag_get_or_create
+from universal.forms import RequestForm
 
 
 class HorizontalRadioRenderer(RadioSelect.renderer):
@@ -122,7 +123,7 @@ class DigestForm(BaseUserForm):
         model = SavedSearchDigest
 
 
-class PartnerSavedSearchForm(ModelForm):
+class PartnerSavedSearchForm(RequestForm):
     def __init__(self, *args, **kwargs):
         choices = PartnerEmailChoices(kwargs.pop('partner', None))
         super(PartnerSavedSearchForm, self).__init__(*args, **kwargs)
@@ -142,10 +143,22 @@ class PartnerSavedSearchForm(ModelForm):
             widget=TextInput(attrs={'id': 'p-tags', 'placeholder': 'Tags'})
         )
 
-        # If it's an edit make the email recipient unchangable (for
-        # compliance purposes).
         if self.instance and self.instance.pk:
+            # If it's an edit make the email recipient unchangeable (for
+            # compliance purposes).
             self.fields['email'].widget.attrs['disabled'] = True
+
+            # Disallow modifying the activation status of a saved search if
+            # ALL of the following are true:
+            # - The current instance has a value stored in unsubscriber
+            # - The value in unsubscriber is an email address used by the
+            #   recipient
+            # - The editor is not the recipient
+            if all([self.instance.unsubscriber,
+                    self.instance.user == User.objects.get_email_owner(
+                        self.instance.unsubscriber),
+                    self.instance.user != self.request.user]):
+                self.fields['is_active'].widget.attrs['disabled'] = True
 
         initial = kwargs.get("instance")
         feed_args = {"widget": HiddenInput()}
@@ -217,6 +230,15 @@ class PartnerSavedSearchForm(ModelForm):
             self._errors.setdefault('url', []).append(error_msg)
 
         self.cleaned_data['feed'] = feed
+
+        if 'is_active' in self.changed_data:
+            self.changed_data.append('unsubscriber')
+            if self.instance.is_active:
+                # Saved search is being deactivated; set unsubscriber
+                self.instance.unsubscriber = self.request.user.email
+            else:
+                # Saved search is being activated; unset unsubscriber
+                self.instance.unsubscriber = ''
         return cleaned_data
 
     def save(self, commit=True):
@@ -246,7 +268,18 @@ class PartnerSavedSearchForm(ModelForm):
         return instance
 
 
-class PartnerSubSavedSearchForm(ModelForm):
+class PartnerSubSavedSearchForm(RequestForm):
+    def clean(self):
+        if 'is_active' in self.changed_data:
+            #self.changed_data.append('unsubscriber')
+            if self.instance.is_active:
+                # Saved search is being deactivated; set unsubscriber
+                self.instance.unsubscriber = self.request.user.email
+            else:
+                # Saved search is being activated; unset unsubscriber
+                self.instance.unsubscriber = ''
+        return self.cleaned_data
+
     class Meta:
         model = PartnerSavedSearch
         fields = ('sort_by', 'frequency', 'day_of_month', 'day_of_week',
--- mysearches/tests/test_forms.py
+++ mysearches/tests/test_forms.py
@@ -1,16 +1,21 @@
 from mock import patch
 
+from django.conf import settings
 from django.core import mail
+from django.core.urlresolvers import reverse
+from django.test import RequestFactory
 
 from myjobs.tests.setup import MyJobsBase
 from mypartners.models import Contact
 from mypartners.tests.factories import ContactFactory, PartnerFactory
-from mysearches.forms import SavedSearchForm, PartnerSavedSearchForm
+from mysearches.forms import SavedSearchForm, PartnerSavedSearchForm, \
+    PartnerSubSavedSearchForm
 from mysearches.forms import PartnerSavedSearch
 from mysearches.tests.helpers import return_file
 from mysearches.tests.factories import SavedSearchFactory
 from myjobs.tests.factories import UserFactory
 from registration.models import Invitation
+from seo.models import SeoSite
 from seo.tests import CompanyFactory
 
 
@@ -79,6 +84,9 @@ class PartnerSavedSearchFormTests(MyJobsBase):
         self.user = UserFactory()
         self.company = CompanyFactory()
         self.partner = PartnerFactory(owner=self.company)
+
+        self.contact = ContactFactory(user=None, email='new_user@example.com',
+                                      partner=self.partner)
         self.partner_search_data = {
             'url': 'http://www.my.jobs/jobs',
             'feed': 'http://www.my.jobs/jobs/feed/rss?',
@@ -86,7 +94,24 @@ class PartnerSavedSearchFormTests(MyJobsBase):
             'label': 'All jobs from www.my.jobs',
             'sort_by': 'Relevance',
             'jobs_per_email': 5,
-        }
+            'email': self.contact.email,
+            }
+
+        settings.SITE = SeoSite.objects.first()
+        self.request = RequestFactory().get(
+            reverse('partner_savedsearch_save'))
+        self.request.user = self.user
+
+        form = PartnerSavedSearchForm(partner=self.partner,
+                                      data=self.partner_search_data,
+                                      request=self.request)
+        instance = form.instance
+        instance.provider = self.company
+        instance.partner = self.partner
+        instance.created_by = self.user
+        instance.custom_message = instance.partner_message
+        self.assertTrue(form.is_valid())
+        form.save()
 
         self.patcher = patch('urllib2.urlopen', return_file())
         self.mock_urlopen = self.patcher.start()
@@ -104,22 +129,10 @@ class PartnerSavedSearchFormTests(MyJobsBase):
         Saving a partner saved search form should also create
         an invitation
         """
-        contact = ContactFactory(user=None, email='new_user@example.com',
-                                 partner=self.partner)
-        self.partner_search_data['email'] = 'new_user@example.com'
-        form = PartnerSavedSearchForm(partner=self.partner,
-                                      data=self.partner_search_data)
-        instance = form.instance
-        instance.provider = self.company
-        instance.partner = self.partner
-        instance.created_by = self.user
-        instance.custom_message = instance.partner_message
-        self.assertTrue(form.is_valid())
-        form.save()
         self.assertEqual(Invitation.objects.count(), 1)
         invitation = Invitation.objects.get()
         self.assertTrue(invitation.invitee.in_reserve)
-        contact = Contact.objects.get(pk=contact.pk)
+        contact = Contact.objects.get(pk=self.contact.pk)
         self.assertEqual(invitation.invitee, contact.user)
 
     def test_initial_email_has_unsubscription_options(self):
@@ -127,21 +140,6 @@ class PartnerSavedSearchFormTests(MyJobsBase):
         The initial email received for a partner saved search should have an
         unsubscribe link included
         """
-
-        # create and submit the form
-        contact = ContactFactory(user=None, email='new_user@example.com',
-                                 partner=self.partner)
-        self.partner_search_data['email'] = 'new_user@example.com'
-        form = PartnerSavedSearchForm(partner=self.partner,
-                                      data=self.partner_search_data)
-        instance = form.instance
-        instance.provider = self.company
-        instance.partner = self.partner
-        instance.created_by = self.user
-        instance.custom_message = instance.partner_message
-        self.assertTrue(form.is_valid())
-        form.save()
-
         # ensure email received with the correct content
         for phrase in ["Deactivate this saved search",
                        "Deactivate all saved searches",
@@ -149,18 +147,28 @@ class PartnerSavedSearchFormTests(MyJobsBase):
             self.assertIn(phrase, mail.outbox[0].body)
 
     def test_sort_by_date_initially(self):
-        contact = ContactFactory(user=None, email='new_user@example.com',
-                                 partner=self.partner)
-        self.partner_search_data['email'] = 'new_user@example.com'
-        form = PartnerSavedSearchForm(partner=self.partner,
-                                      data=self.partner_search_data)
-        instance = form.instance
-        instance.provider = self.company
-        instance.partner = self.partner
-        instance.created_by = self.user
-        instance.custom_message = instance.partner_message
-        self.assertTrue(form.is_valid())
-        form.save()
-
         instance = PartnerSavedSearch.objects.get()
         self.assertEqual(instance.sort_by, 'Date')
+
+    def test_disable_partner_saved_search(self):
+        pss = PartnerSavedSearch.objects.get()
+
+        form = PartnerSavedSearchForm(instance=pss, request=self.request,
+                                      data=self.partner_search_data)
+        self.assertFalse(form.fields['is_active'].widget.attrs.get('disabled',
+                                                                   False))
+
+        pss.unsubscriber = pss.email
+        pss.save()
+
+        form = PartnerSavedSearchForm(instance=pss, request=self.request,
+                                      data=self.partner_search_data)
+        self.assertTrue(form.fields['is_active'].widget.attrs.get('disabled',
+                                                                  False))
+
+        self.request.user = pss.user
+        form = PartnerSubSavedSearchForm(instance=pss, request=self.request,
+                                         data=self.partner_search_data)
+        self.assertFalse(form.fields['is_active'].widget.attrs.get('disabled',
+                                                                   False))
+        self.assertTrue(form.is_valid())
