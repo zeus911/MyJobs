d40818cc152bd654502603ea541cdaf07d94a3f1
aspidites <emarshall85@gmail.com>
2015-04-07 10:14:44 -0400

    HTML properly stripped from columns now.

--- myreports/helpers.py
+++ myreports/helpers.py
@@ -114,21 +114,21 @@ def serialize(fmt, data, counts=None, values=None):
 
         # Cant' use a ValueQuerSet for serialize, which means we lose the
         # ability to use distinct. As such, we fake it by doing so manually
-        if values:
-            records = []
-            haystack = []
+        records = []
+        haystack = []
 
-            for record in data:
-                needle = [record[value] for value in values] or record['pk']
+        for record in data:
+            needle = [record[value]
+                      for value in values] if values else record['pk']
 
-                if needle not in haystack:
-                    haystack.append(needle)
-                    # strip HTML tags from string values
-                    records.append({
-                        key: strip_tags(value) if isinstance(value, basestring)
-                        else value for key, value in record.items()})
+            if needle not in haystack:
+                haystack.append(needle)
+                # strip HTML tags from string values
+                records.append({
+                    key: strip_tags(value) if isinstance(value, basestring)
+                    else value for key, value in record.items()})
 
-            data = records
+        data = records
 
     if fmt == 'json':
         return json.dumps(data, cls=DjangoJSONEncoder)
