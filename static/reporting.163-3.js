f01ca39aa28ad94b77d94f21ebde77ee41174a6c
David Galitsky <galitskyd@gmail.com>
2015-04-07 15:07:28 -0400

    fixed pushStates being called by popStates

--- static/reporting.163-3.js
+++ static/reporting.163-3.js
@@ -728,6 +728,8 @@ $(document).ready(function() {
       report_id = $(this).parents("tr").data("report");
     }
 
+    history.pushState({'page': 'view-report', 'reportId': report_id}, 'View Report');
+
     renderGraphs(report_id);
   });
 
@@ -766,6 +768,7 @@ $(document).ready(function() {
 
   // View Archive
   subpage.on("click", "#report-archive", function() {
+    history.pushState({'page': 'report-archive'}, "Report Archive");
     renderArchive();
   });
 });
@@ -918,8 +921,6 @@ function renderGraphs(report_id) {
                         1: {'name': "Interviews",   'count': interviews,   'style': "color: #4BB1CF"},
                         2: {'name': "Hires",        'count': hires,        'style': "color: #FAA732"},
                         3: {'name': "Records",      'count': referrals,    'style': "color: #5F6C82"}};
-
-      history.pushState({'page': 'view-report', 'reportId': report_id}, 'View Report');
 
       // Grab google's jsapi to load chart files.
       $.getScript("https://www.google.com/jsapi", function() {
@@ -1089,7 +1090,6 @@ function renderArchive(callback) {
     url: "archive",
     data: {},
     success: function(data) {
-      history.pushState({'page': 'report-archive'}, "Report Archive");
       $("#main-container").html(data);
     }
   }).complete(function() {
