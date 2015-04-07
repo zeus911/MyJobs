016129ff35c3289ec334a118c73fb74a281ae007
aspidites <emarshall85@gmail.com>
2015-04-07 16:18:18 -0400

    enabled handles

--- static/reporting.162-26.js
+++ static/reporting.162-26.js
@@ -819,7 +819,7 @@ function renderDownload(report_id) {
       $("#download-report").attr("href", "download?" + $.param(ctx));
 
       $(".card-holder").sortable({
-        //"handle": "span.fa-bars",
+        "handle": "span.fa-bars",
         "axis": "y",
         "update": function(e, ui) {
           values = $.map($(".enable-column:checked"), function(item, index) {
