a205f4b194a9f3319eee8983fe82d7d71233759f
aspidites <emarshall85@gmail.com>
2015-04-09 08:51:05 -0400

    Replaced spaces with underscores when creating a report file.

--- myreports/views.py
+++ myreports/views.py
@@ -185,7 +185,7 @@ class ReportView(View):
             params = parse_params(request.POST)
 
             params.pop('csrfmiddlewaretoken', None)
-            name = params.pop('report_name', datetime.now())
+            name = params.pop('report_name', datetime.now()).replace(' ', '_')
             values = params.pop('values', None)
 
             records = get_model(app, model).objects.from_search(
