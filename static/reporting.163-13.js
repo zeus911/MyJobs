0bf50766ca4e2cffa5276ec8c21585bdbab28e63
David Galitsky <galitskyd@gmail.com>
2015-04-14 15:41:58 -0400

    added new object Field

--- static/reporting.163-13.js
+++ static/reporting.163-13.js
@@ -15,6 +15,16 @@ var Report = function(types) {
 };
 
 
+var Field = function(report, label, id, required, defaultVal, helpText) {
+  this.report = report;
+  this.label = label;
+  this.id = id;
+  this.required = !!required || false;
+  this.defaultVal = defaultVal || '';
+  this.helpText = helpText;
+};
+
+
 // Checks to see if browser is IE. If it is then get version.
 function isIE() {
     var myNav = navigator.userAgent.toLowerCase();
