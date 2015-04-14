4ac2e2020edd13f5c0805f4aba1b574927c2d1a9
aspidites <emarshall85@gmail.com>
2015-04-14 11:30:46 -0400

    Added documentation.

--- static/prm-forms.163-8.js
+++ static/prm-forms.163-8.js
@@ -1,3 +1,23 @@
+/* 
+ * This file is only concerned with the partner and contact forms in PRM.
+ * However, it seemed like a bit of a stretch to have to name it
+ * 'prm-partner-and-contact-form-only.xxx-x.js'.
+ */
+
+/*
+ * Checks if the vaue of an input is already in an array of objects
+ *
+ * Inputs:
+ * :elementId: The element to grab by ID
+ * :values: The list of objects to check against for duplicates. Also has a
+ *          number of attributes used for display, such as email, uri, etc. 
+ *  :url: The url to attach the element's primary key to. This url is used to
+ *        take the user to the already existing item.
+ *
+ *  Outputs:
+ *  This function returns a boolean denoting whether or not a duplicate was
+ *  found.
+ */
 function checkDuplicates(elementID, values, url) {
   var $element = $(elementID),
       names = $.map(values, function(item, index) { return item.name.toLowerCase(); }),
@@ -5,6 +25,7 @@ function checkDuplicates(elementID, values, url) {
       $editModal = $("#edit-item-modal");
 
   if (index > -1) {
+    // Create an unordered list of the pre-existing item's attributes.
     $("ul.record-info").html(
       "<li>Name: " + values[index].name + "</li>" +
       (values[index].uri ? "<li>URL: " + values[index].uri + "</li>" : "") +
@@ -12,8 +33,10 @@ function checkDuplicates(elementID, values, url) {
       (values[index].phone ? "<li>Phone: " + values[index].phone + "</li>" : "")
     );
 
+    // Update the href for hte edit button.
     $("#edit-record-button").attr("href", url + values[index].pk);
 
+    // Return focus to the element that contains duplicate data.
     $editModal.modal("show").on("hidden", function() {
       $element.select();
     });
@@ -25,10 +48,12 @@ function checkDuplicates(elementID, values, url) {
 
 $(document).ready(function() {
   var $otherInputs = $("input:gt(0)"),
-      $saveButton = $("#item-save"),
+      $saveButton = $("#item-save, #init-partner-save"),
       validForm = false;
   
   $otherInputs.on("focus", function() {
+    // Which input is present determines which form we are viewing, and thus
+    // which parameters we need to send to checkDuplicates.
     if ($("#id_partner-partnername").length) {
       checkDuplicates("#id_partner-partnername", partners, "/prm/view/details?partner=");
     } else if ($("#id_contact-name").length) {
@@ -36,7 +61,12 @@ $(document).ready(function() {
     }
   });
 
+  // If the save button doesn't exist, we are editing a partner or contact,
+  // which doens't require this special behavior.
   if ($saveButton.length) {
+    // prm.x-x.js has different handlers depending on which form we are on.
+    // Here, we are preserving that callback, detatching it, then re-attaching
+    // it later if the form turns out to be valid
     oldCallback = $._data($saveButton[0], "events").click[0];
     $saveButton.unbind("click");
     $saveButton.on("click", function(e) {
@@ -48,8 +78,10 @@ $(document).ready(function() {
         validForm = checkDuplicates("#id_contact-name", contacts, "/prm/view/details/edit?partner=" + partner + "&id=");
       }
 
+      // Restore the original callback behavior for the save button
       if (validForm) {
         $saveButton.unbind("click").bind("click", oldCallback).click();
       }
     });
   }
+});
