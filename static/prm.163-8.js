7159932497e891c148e1b0a70b91ce0a183661a3
aspidites <emarshall85@gmail.com>
2015-04-14 07:55:23 -0400

    Made the handler for save button a global callback function.

--- static/prm.163-8.js
+++ static/prm.163-8.js
@@ -208,67 +208,8 @@ $(document).ready(function() {
         });
     });
 
-    $('#item-save, #contact-save').on("click", function(e) {
-        // interrupts default functionality of the button with code below
-        e.preventDefault();
 
-        var is_c_form_there = $('#contact-form').length;
-        if (is_c_form_there > 0) {
-            var form = $('#contact-form');
-        }
-        else {
-            var form = $('#partner-form');
-        }
-
-        var serialized_data = form.serialize();
-
-        var get_data = window.location.search;
-        if (get_data.length) {
-            get_data = '&' + get_data.substr(1);
-        }
-        serialized_data += get_data + '&ct=' + $('[name=ct]').val();
-
-        var company_id = $('[name=company_id]').val();
-        var partner_id = $('[name=partner_id]').val();
-
-        $.ajax({
-            type: 'POST',
-            url: '/prm/view/details/save',
-            data: serialized_data,
-            success: function(data, status) {
-
-                if (data == ''){
-                    if (status != 'prevent-redirect') {
-                        window.location = '/prm/view/details?partner=' + partner_id;
-                    }
-                } else {
-                    // form was a json-encoded list of errors and error messages
-                    var json = jQuery.parseJSON(data);
-
-                    // remove color from labels of current errors
-                    $('[class*=required]').parent().prev().removeClass('error-text');
-
-                    // remove current errors
-                    $('[class*=required]').children().unwrap();
-
-                    if($.browser.msie){
-                        $('[class*=msieError]').remove()
-                    }
-
-                    for (var index in json) {
-                        var $error = $('[id$="-'+index+'"]');
-                        var $labelOfError = $error.parent().prev();
-
-                        // insert new errors after the relevant inputs
-                        $error.wrap('<div class="required" />');
-                        $error.attr("placeholder",json[index][0]);
-                        $error.val('');
-                        $labelOfError.addClass('error-text');
-                    }
-                }
-            }
-        });
-    });
+    $('#item-save, #contact-save').on("click", callback);
 
     // Onclick span will check the checkbox it is next to
     $("#library-modal-check span").on("click", function() {
@@ -810,6 +751,68 @@ function update_search_url(data) {
         location.search = search_url;
     }
 }
+
+function (e) {
+    // interrupts default functionality of the button with code below
+    e.preventDefault();
+
+    var is_c_form_there = $('#contact-form').length;
+    if (is_c_form_there > 0) {
+        var form = $('#contact-form');
+    }
+    else {
+        var form = $('#partner-form');
+    }
+
+    var serialized_data = form.serialize();
+
+    var get_data = window.location.search;
+    if (get_data.length) {
+        get_data = '&' + get_data.substr(1);
+    }
+    serialized_data += get_data + '&ct=' + $('[name=ct]').val();
+
+    var company_id = $('[name=company_id]').val();
+    var partner_id = $('[name=partner_id]').val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/prm/view/details/save',
+        data: serialized_data,
+        success: function(data, status) {
+
+            if (data == ''){
+                if (status != 'prevent-redirect') {
+                    window.location = '/prm/view/details?partner=' + partner_id;
+                }
+            } else {
+                // form was a json-encoded list of errors and error messages
+                var json = jQuery.parseJSON(data);
+
+                // remove color from labels of current errors
+                $('[class*=required]').parent().prev().removeClass('error-text');
+
+                // remove current errors
+                $('[class*=required]').children().unwrap();
+
+                if($.browser.msie){
+                    $('[class*=msieError]').remove()
+                }
+
+                for (var index in json) {
+                    var $error = $('[id$="-'+index+'"]');
+                    var $labelOfError = $error.parent().prev();
+
+                    // insert new errors after the relevant inputs
+                    $error.wrap('<div class="required" />');
+                    $error.attr("placeholder",json[index][0]);
+                    $error.val('');
+                    $labelOfError.addClass('error-text');
+                }
+            }
+        }
+    });
+};
 
 /*
 If someone loads the page with request.GET info (not from ajax) fill page with info
