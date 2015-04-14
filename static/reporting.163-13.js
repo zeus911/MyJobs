c1dab60ac747968a9ba21c5ad40a85cf22859c6d
David Galitsky <galitskyd@gmail.com>
2015-04-14 16:30:50 -0400

    added testing things

--- static/reporting.163-13.js
+++ static/reporting.163-13.js
@@ -18,7 +18,9 @@ var Report = function(types) {
 Report.prototype.createFields = function(types) {
   var fields = {"prm": [new TextField(this, "Required1", "required1", true),
                         new TextField(this, "Not Required", "notrequired1", false),
-                        new TextField(this, "Default Value 5", "defaultvalue", false, "5")]};
+                        new TextField(this, "Default Value 5", "defaultvalue", false, "5"),
+                        new StateField(this, "State", 'state', false, 'IN'),
+                        new DateField(this, "Select Date", "date", true, {start_date: "02/23/1991", end_date: "04/14/2015"})]};
 
 
   return fields[types[0]];
@@ -54,6 +56,7 @@ Report.prototype.findField = function(fieldID) {
 Report.prototype.save = function() {
   var field,
       i;
+
   for(i = 0; i < this.fields.length; i++) {
     field = this.fields[i];
     $.extend(this.data, field.onSave());
@@ -230,6 +233,16 @@ StateField.prototype.render = function() {
   })();
   return label + '<div class="state"></div>';
 };
+
+
+$(document).ready(function() {
+  $("body").append('<a id="test" class="btn">Click me!</a>').append('<div class="meh"></div>');
+  $("#test").on("click", function() {
+    $("#container").html("").addClass("rpt-container");
+    report = new Report(['prm']);
+    report.renderFields(".rpt-container", report.fields);
+  });
+});
 
 
 // Checks to see if browser is IE. If it is then get version.
