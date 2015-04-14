7055fd7ae4b7a87f5ab278ae70449077df34bdb9
David Galitsky <galitskyd@gmail.com>
2015-04-14 16:28:51 -0400

    added DateField functions

--- static/reporting.163-13.js
+++ static/reporting.163-13.js
@@ -159,6 +159,51 @@ var DateField = function(report, label, id, required, defaultVal, helpText) {
 DateField.prototype = Object.create(Field.prototype);
 
 
+DateField.prototype.render = function() {
+  var label = this.renderLabel(),
+      dateWidget = $("<div id='" + this.id + "' class='filter-option'><div class='date-picker'></div></div>"),
+      datePicker = $(dateWidget).find(".date-picker"),
+      to = "<span id='activity-to-' class='datepicker'>to</span>",
+      start = "<input id='start-date' class='datepicker picker-left' type='text' value='" + (this.defaultVal ? this.defaultVal.start_date : "") + "' placeholder='Start Date' />",
+      end = "<input id='end-date' class='datepicker picker-right' type='text' value='" + (this.defaultVal ? this.defaultVal.end_date : "")  + "' placeholder='End Date' />";
+
+  datePicker.append(start).append(to).append(end);
+  dateWidget.append(datePicker);
+  return label + dateWidget.prop("outerHTML");
+};
+
+
+DateField.prototype.bindEvents = function() {
+  var $element = $(this.dom());
+  $element.on("focus.datepicker", ".datepicker", function() {
+    $(this).pickadate({
+      format: "mm/dd/yyyy",
+      selectYears: true,
+      selectMonths: true,
+      today: false,
+      clear: false,
+      close: false,
+      onOpen: function () {
+        if (this.get("id") === "start-date") {
+          var end_date = $("#end-date").val();
+          this.set("max", new Date(end_date || new Date()));
+        } else if (this.get("id") === "end-date") {
+          var start_date = $("#start-date").val();
+          this.set("min", new Date(start_date || new Date(0)));
+        }
+      }
+    });
+  });
+};
+
+
+DateField.prototype.onSave = function() {
+  var data = {};
+  data.start_date = '';
+  return data;
+};
+
+
 var StateField = function(report, label, id, required, defaultVal, helpText) {
   Field.call(this, report, label, id, required, defaultVal, helpText);
 };
