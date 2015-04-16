f05a56b52ebccc9415b1654994b30d407b8b567b
tdphillips <troy@directemployersfoundation.org>
2015-04-16 13:30:44 -0400

    Fixes for saved search user nullification [PD-560]
    - Only show the "Send" button if the user isn't None
    - Slightly condense some conditionals around user checking

--- mysearches/models.py
+++ mysearches/models.py
@@ -324,8 +324,7 @@ class SavedSearch(models.Model):
         Create a new saved search digest if one doesn't exist yet
         """
 
-        if (self.user is not None and
-                not SavedSearchDigest.objects.filter(user=self.user)):
+        if self.user and not SavedSearchDigest.objects.filter(user=self.user):
             SavedSearchDigest.objects.create(user=self.user, email=self.email)
 
         super(SavedSearch, self).save(*args, **kwargs)
