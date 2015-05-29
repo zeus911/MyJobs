fd40b2c85e923b1965baed779e751a9bfd9d8acc
David Galitsky <galitskyd@gmail.com>
2015-05-29 16:21:44 -0400

    new urls views and templates

--- myemails/urls.py
+++ myemails/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import patterns, url
 
+from myemails.views import (Overview, ViewTemplate, IframeView, EditTemplate,
+                            ManageHeaderFooter, manage_template)
+
 urlpatterns = patterns(
     'myemails.views',
     url(r'^get-fields/$', 'get_fields', name='get_fields'),
+    url(r'view/$', Overview.as_view(), name='email-overview'),
+    url(r'view/manage/view$', ManageHeaderFooter.as_view(), name='manage-h-f'),
+    url(r'view/template/manage$', manage_template, name='manage-template'),
+    url(r'view/template/edit$', EditTemplate.as_view(), name='edit-template'),
+    url(r'view/template/view$', ViewTemplate.as_view(), name='view-template'),
+    url(r'view/template/iframe$', IframeView.as_view(), name='iframe-view'),
 )
--- myemails/views.py
+++ myemails/views.py
@@ -1,6 +1,10 @@
+import json
+
 from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponse
-import json
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.views.generic import TemplateView
 
 
 def get_fields(request):
@@ -8,3 +12,31 @@ def get_fields(request):
     content_type = ContentType.objects.get(pk=model)
     model_class = content_type.model_class()
     return HttpResponse(json.dumps(getattr(model_class, 'EVENT_FIELDS', {})))
+
+
+class Overview(TemplateView):
+    template_name = "myemails/email-overview.html"
+
+
+class ManageHeaderFooter(TemplateView):
+    template_name = "myemails/footer-header.html"
+
+
+def manage_template(request):
+    ctx = {
+        "mmm": ["Event1", "Event2", "Event3", "Event4"]
+    }
+    return render_to_response('myemails/manage-template.html',
+                              ctx, RequestContext(request))
+
+
+class ViewTemplate(TemplateView):
+    template_name = "myemails/view-template.html"
+
+
+class IframeView(TemplateView):
+    template_name = "myemails/iframe-view.html"
+
+
+class EditTemplate(TemplateView):
+    template_name = "myemails/edit-template.html"
